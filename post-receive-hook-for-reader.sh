f4f0a2b30b62319489fae07fe84d74d97d1f54c6
Denys Vachov <dvac@isd.dp.ua>
2014-03-26 11:14:00 +0200

    changes in post-receive-hook-for-reader.sh

--- post-receive-hook-for-reader.sh
+++ post-receive-hook-for-reader.sh
@@ -13,18 +13,21 @@ then
         list=($( git for-each-ref --format="%(refname)" refs/heads | sed 's/refs\/heads\///g'))
         cat /dev/null > $FILE
         echo '{' >> $FILE
+        echo -e '\t"branches":' >> $FILE
+        echo -e '\t\t[' >> $FILE
         counter=0
         a=$(( ${#list[@]} -1 ))
         while (( $counter < $a ))
         do
-                echo -e '\t"'${list[$counter]}'",' >> $FILE
+                echo -e '\t\t\t"'${list[$counter]}'",' >> $FILE
                 ((counter++))
         done
         if [ "$counter" -eq "$a" ]
         then
-                echo -e '\t"'${list[@]:(-1)}'"' >> $FILE
+                echo -e '\t\t\t"'${list[@]:(-1)}'"' >> $FILE
                 ((counter++))
         fi
+        echo -e '\t\t]' >> $FILE
         echo '}'  >> $FILE
 fi
 
