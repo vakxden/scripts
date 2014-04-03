17f8e7a84d1b3ccb4f55f7e8a292e34bf8656b8d
Denys Vachov <dvac@isd.dp.ua>
2014-04-03 20:42:20 +0300

    added functional for generate index.html

--- portgenerator-for-deploy.sh
+++ portgenerator-for-deploy.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-BRANCHNAME="$1"
+ANCHNAME="$1"
 FACETS="$2"
 dest="$3"
 ID="$4"
@@ -71,18 +70,43 @@ echo -e '\t"database_name": "'$FACETS'"' >> local.json
 echo '}'  >> local.json
 
 ### Touch apache config file
-ACF="irls-$CURRENT-reader-$FACETS-$BRANCHNAME"
+ACF="/etc/apache2/sites-enabled/irls-$CURRENT-reader-$FACETS-$BRANCHNAME"
 rm -f $ACF
 touch $ACF
 if [ -z $ID ]; then
          echo "ID was not passed"
 else
-        echo -e '\t'ProxyPass /irls/$CURRENT/reader/$FACETS/$BRANCHNAME.artifacts  http://127.0.0.1/$ARTDIR/$ID/packages/ >> $ACF
-        echo -e '\t'ProxyPassReverse /irls/$CURRENT/reader/$FACETS/$BRANCHNAME.artifacts  http://127.0.0.1/$ARTDIR/$ID/packages/ >> $ACF
+        if [ ! -d /home/jenkins/$ARTDIR/$ID/packages/artifacts ]; then
+                mkdir -p /home/jenkins/$ARTDIR/$ID/packages/artifacts && chown -Rf jenkins:jenkins /home/jenkins/$ARTDIR/$ID/packages/artifacts
+        fi
+        echo -e '\t'ProxyPass /irls/$CURRENT/reader/$FACETS/$BRANCHNAME.artifacts  http://127.0.0.1/$ARTDIR/$ID/packages/artifacts/ >> $ACF
+        echo -e '\t'ProxyPassReverse /irls/$CURRENT/reader/$FACETS/$BRANCHNAME.artifacts  http://127.0.0.1/$ARTDIR/$ID/packages/artifacts/ >> $ACF
+                ### generate index.html
+                cd /home/jenkins/$ARTDIR/$ID/packages/artifacts/
+                cat /dev/null > index.html
+                echo -e '<!DOCTYPE HTML>' >> index.html
+                echo -e '<html><head><title>List of artifacts</title></head>' >> index.html
+                echo -e '<body><h1>List of artifacts</h1>' >> index.html
+                echo -e '<table><tr><th><a href="?C=N;O=D">Name</a></th><th><a href="?C=M;O=A">Last modified</a></th><th><a href="?C=S;O=A">Size</a></th></tr><tr><th colspan="5"><hr></th></tr>' >> index.html
+                if [ -f "$(find . -name *.ipa)" ]; then
+                        for name in $(find . -name *.ipa)
+                        do
+                                DATE=$(stat -c %y $name | awk '{print $1,$2}' | awk -F'.' '{print $1}')
+                                SIZE=$(($(stat -c %s $name)/1048576))
+                                echo -e '<tr><td><a href="'$name'">'$name'</a></td><td align="right">'$DATE'</td><td align="right">'$SIZE'MB</td><td>&nbsp;</td></tr>' >> index.html
+                        done
+                fi
+                if [ -f "$(find . -name *.zip)" ]; then
+                        for name in $(find . -name *.zip)
+                        do
+                                DATE=$(stat -c %y $name | awk '{print $1,$2}' | awk -F'.' '{print $1}')
+                                SIZE=$(($(stat -c %s $name)/1048576))
+                                echo -e '<tr><td><a href="'$name'">'$name'</a></td><td align="right">'$DATE'</td><td align="right">'$SIZE'MB</td><td>&nbsp;</td></tr>' >> index.html
+                        done
+                fi
+                echo -e '<tr><th colspan="5"><hr></th></tr></table></body></html>' >> index.html
 fi
 
 echo -e '\t'ProxyPass /irls/$CURRENT/reader/$FACETS/$BRANCHNAME/ http://127.0.0.1:$GENERATED_PORT/ >> $ACF
 echo -e '\t'ProxyPassReverse /irls/$CURRENT/reader/$FACETS/$BRANCHNAME/ http://127.0.0.1:$GENERATED_PORT/ >> $ACF
-rm -f /etc/apache2/sites-enabled/$ACF
-cp $ACF /etc/apache2/sites-enabled/$ACF
 service apache2 reload
