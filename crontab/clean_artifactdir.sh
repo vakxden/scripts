94c6482503f9bd6795c4db49454d725bcf29beb7
Denys Vachov <dvac@isd.dp.ua>
2014-05-13 11:13:25 +0300

    add clean block for stage artifacts directory

--- crontab/clean_artifactdir.sh
+++ crontab/clean_artifactdir.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-while (($(df -h /fs/backup/ | tail -1 | awk '{print $2}' | sed 's/G//g')>150))
+# Current artifacts directory
+while (($(df -h /fs/backup/ | tail -1 | awk '{print $2}' | sed 's/G//g')>250))
 do
-         echo "used more then 150 Gb"
-        for i in $(ls -lahtr /home/jenkins/irls-reader-artifacts/ | head -6 | awk '{print $9}')
+        printf " /fs/backup/ used more then 250 Gb\n"
+        for i in $(ls -lahtr /home/jenkins/irls-reader-artifacts/ | egrep -v "branches.json|trendcountbooks.png|environment.json" | head -6 | awk '{print $9}')
         do
                 rm -rf /home/jenkins/irls-reader-artifacts/$i
+        done
+done
+
+# Stage artifacts directory
+while (($(df -h /home | tail -1 | awk '{print $2}' | sed 's/G//g')>160))
+do
+        echo "/home used more then 160 Gb\n"
+        for i in $(ls -lahtr /home/jenkins/irls-reader-artifacts-stage/ | head -6 | awk '{print $9}')
+        do
+                rm -rf /home/jenkins/irls-reader-artifacts-stage/$i
         done
 done
