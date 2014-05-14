817fb953905bf93ae0f44ddb7fb896925b4c8e9a
Denys Vachov <dvac@isd.dp.ua>
2014-05-14 11:53:37 +0300

    some changes in LA graph generate

--- crontab/load.sh
+++ crontab/load.sh
@@ -16,42 +16,40 @@ case $1 in (create)
                 N:`/bin/sed "s/\([0-9]\\.[0-9]\\{2\\}\)\ \([0-9]\\.[0-9]\\{2\\}\)\ \([0-9]\\.[0-9]\\{2\\}\).*/\1:\2:\3/" < /proc/loadavg`:`/usr/bin/head -n 1 /proc/stat | /bin/sed "s/^cpu\ \+\([0-9]*\)\ \([0-9]*\)\ \([0-9]*\).*/\1:\2:\3/"`;;
         (graph)
                 /usr/bin/rrdtool graph /var/www/load.png \
-                -Y -u 1.1 -l 0 -L 5 -v "Load" -w 700 -h 300 -t "Load & CPU stats - `/bin/date`" \
+                --y-grid='1:5' -v "Load Average (LA)" --width 1200 --height 400 -t "Load & CPU stats - `/bin/date`" \
                 -c ARROW\#000000 -x MINUTE:30:MINUTE:30:HOUR:1:0:%H \
                 DEF:load1=/var/db/rrdtool/load.rrd:load1:AVERAGE \
                 DEF:load5=/var/db/rrdtool/load.rrd:load5:AVERAGE \
                 DEF:load15=/var/db/rrdtool/load.rrd:load15:AVERAGE \
-                DEF:user=/var/db/rrdtool/load.rrd:cpuuser:AVERAGE \
-                DEF:nice=/var/db/rrdtool/load.rrd:cpunice:AVERAGE \
-                DEF:sys=/var/db/rrdtool/load.rrd:cpusystem:AVERAGE \
-                CDEF:cpu=user,nice,sys,+,+ \
-                CDEF:reluser=load15,user,100,/,* \
-                CDEF:relnice=load15,nice,100,/,* \
-                CDEF:relsys=load15,sys,100,/,* \
-                CDEF:idle=load15,100,cpu,-,100,/,* \
-                HRULE:1\#000000 \
                 COMMENT:"       " \
-                AREA:reluser\#FF0000:"CPU user" \
-                STACK:relnice\#00AAFF:"CPU nice" \
-                STACK:relsys\#FFFF00:"CPU system" \
-                STACK:idle\#00FF00:"CPU idle" \
-                COMMENT:"       \j" \
-                COMMENT:"       " \
-                LINE1:load1\#000FFF:"Load average 1 min" \
-                LINE2:load5\#000888:"Load average 5 min" \
-                LINE3:load15\#000000:"Load average 15 min" \
+                LINE1:load1#EE3B3B:"Load average 1 min" \
+                LINE2:load5#7D26CD:"Load average 5 min" \
+                LINE3:load15#00CD66:"Load average 15 min" \
                 COMMENT:"       \j" \
                 COMMENT:"\j" \
                 COMMENT:"       " \
-                GPRINT:load15:MIN:"Load 15 min minimum\: %lf" \
-                GPRINT:load15:MAX:"Load 15 min maximum\: %lf" \
-                GPRINT:load15:AVERAGE:"Load 15 min average\: %lf" \
+                GPRINT:load1:MIN:"LA1 min\: %lf" \
+                GPRINT:load5:MIN:"LA5 min\: %lf" \
+                GPRINT:load15:MIN:"LA15 min\: %lf" \
                 COMMENT:"       \j" \
+                COMMENT:"\j" \
                 COMMENT:"       " \
-                GPRINT:cpu:MIN:"CPU usage minimum\: %lf%%" \
-                GPRINT:cpu:MAX:"CPU usage maximum\: %lf%%" \
-                GPRINT:cpu:AVERAGE:"CPU usage average\: %lf%%" \
-                COMMENT:"       \j";
+                GPRINT:load1:MAX:"LA1 max\: %lf" \
+                GPRINT:load5:MAX:"LA5 max\: %lf" \
+                GPRINT:load15:MAX:"LA15 max\: %lf" \
+                COMMENT:"       \j" \
+                COMMENT:"\j" \
+                COMMENT:"       " \
+                GPRINT:load1:AVERAGE:"LA1 avg\: %lf" \
+                GPRINT:load5:AVERAGE:"LA5 avg\: %lf" \
+                GPRINT:load15:AVERAGE:"LA15 avg\: %lf" \
+                COMMENT:"       \j" \
+                GPRINT:load1:LAST:"Last LA1\: %2.1lf " \
+                COMMENT:"       \j" \
+                GPRINT:load5:LAST:"Last LA5\: %2.1lf " \
+                COMMENT:"       \j" \
+                GPRINT:load15:LAST:"Last LA15\: %2.1lf " \
+                COMMENT:"       \j" \
                 #
                 /usr/bin/rrdtool graph /var/www/cpu.png \
                 -Y -r -u 100 -l 0 -L 5 -v "CPU usage" -w 700 -h 300 -t "Bifroest CPU stats - `/bin/date`" \
