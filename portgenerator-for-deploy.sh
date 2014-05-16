3b3bd942d8c67bd7a4c529ade1142fdd0c4bb6be
Denys Vachov <dvac@isd.dp.ua>
2014-05-16 17:44:06 +0300

    modified portgenerator-for-deploy.sh

--- portgenerator-for-deploy.sh
+++ portgenerator-for-deploy.sh
@@ -74,8 +74,11 @@ function generate_indexhtml {
         echo -e '<table><tr><th><a href="?C=N;O=D">Name</a></th><th><a href="?C=M;O=A">Last modified</a></th><th><a href="?C=S;O=A">Size</a></th></tr><tr><th colspan="5"><hr></th></tr>' >> index.html
         IPAFILE=$1*FFA_Reader*$2*.ipa
         ZIPWINFILE=$1*FFA_Reader*$2-win*.zip
+        ZIPLINUX32FILE=$1*FFA_Reader*$2-linux32*.zip
+        ZIPLINUX64FILE=$1*FFA_Reader*$2-linux64*.zip
+        ZIPMACOSFILE=$1*FFA_Reader*$2-macos*.zip
         APKFILE=$1*FFA_Reader*$2*.apk
-        for file in $IPAFILE $ZIPWINFILE $APKFILE
+        for file in $IPAFILE $ZIPWINFILE $APKFILE $ZIPMACOSFILE $ZIPLINUX32FILE $ZIPLINUX64FILE
         do
                 if [ -f "$(find . -name $file)" ]; then
                         for name in $(find . -name $file)
