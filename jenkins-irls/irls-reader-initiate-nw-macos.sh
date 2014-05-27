f1e498bc8272c9b4354a1c147df2000319d209f8
Denys Vachov <dvac@isd.dp.ua>
2014-05-27 13:20:20 +0300

    add irls-reader-initiate-nw-macos.sh

---
+++ jenkins-irls/irls-reader-initiate-nw-macos.sh
@@ -0,0 +1,48 @@
+###
+### Variables
+###
+BRANCH=$(echo $BRANCHNAME | sed 's/\//-/g' | sed 's/_/-/g')
+ARTIFACTS_DIR=/home/jenkins/irls-reader-artifacts
+CURRENT_EPUBS=$HOME/irls-reader-current-epubs
+#FACETS=(puddle bahaiebooks lake audio mediaoverlay)
+FACETS=($(echo $FACET))
+###
+### Body (working with all facets exclude only facet named "ocean")
+###
+for facet in ${FACETS[@]}
+do
+        if [ $(echo "$facet" | egrep "puddle$|bahaiebooks$|mediaoverlay$|audio$|audiobywords$|lake$") ]; then
+		### Remove old version of project and zip-archives
+		rm -rf client packager server zip/*.zip
+		### Copy project to workspace
+		if [ ! -d zip ]; then mkdir zip; fi
+		cp -Rf $CURRENT_BUILD/$GIT_COMMIT/* .
+		### Create associative array
+		deploymentPackageId=($(echo $ID))
+                ELEMENT_OF_FACETS=($facet)
+                declare -A combineArray
+                for ((x=0; x<${#deploymentPackageId[@]}; x++))
+                do
+                        for ((y=0; y<${#ELEMENT_OF_FACETS[@]}; y++))
+                        do
+                                if [ -n "$(echo "${deploymentPackageId[x]}" | grep "${ELEMENT_OF_FACETS[y]}$")" ]; then
+                                        combineArray+=(["${ELEMENT_OF_FACETS[y]}"]="${deploymentPackageId[x]}")
+                                fi
+                        done
+                done
+		### Create zip-archive with application version for MacOS
+		for i in "${!combineArray[@]}"
+		do
+			echo $i --- ${combineArray[$i]}
+			cd $WORKSPACE/packager
+			node index.js --target=macos --config=/home/jenkins/build_config --from=$WORKSPACE/client --manifest=$WORKSPACE/client/package.json --prefix=$BRANCH- --suffix=-$i --epubs=$CURRENT_EPUBS/$i
+					mv $WORKSPACE/packager/out/dest/*.zip $WORKSPACE/zip/
+			if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts ]; then
+				mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts
+			fi
+			cp $WORKSPACE/zip/*$i-macos*.zip $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts/
+		done
+	else
+		printf "we can only work with the all facets exclude 'ocean' \n"
+	fi
+done
