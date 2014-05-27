a75312c3236e4a2a308dc7269537f601b6f7645d
Denys Vachov <dvac@isd.dp.ua>
2014-05-27 18:00:58 +0300

    execution of the job was switched to dev02.design.isd.dp.ua

--- jenkins-irls/irls-reader-initiate-nw-macos.sh
+++ jenkins-irls/irls-reader-initiate-nw-macos.sh
@@ -1,10 +1,23 @@
 ###
+### This script takes the following parameters: 
+###  BRANCHNAME 
+###  GIT_TSOMMIT 
+###  CURRENT_BUILD 
+###  ID 
+###  FACET
+###
+
+###
+### path to node (because this job working in host dev02.design.isd.dp.ua)
+###
+export NODE_HOME=/opt/node
+export PATH=$PATH:$NODE_HOME/bin/
+###
 ### Variables
 ###
 BRANCH=$(echo $BRANCHNAME | sed 's/\//-/g' | sed 's/_/-/g')
 ARTIFACTS_DIR=/home/jenkins/irls-reader-artifacts
 CURRENT_EPUBS=$HOME/irls-reader-current-epubs
-#FACETS=(puddle bahaiebooks lake audio mediaoverlay)
 FACETS=($(echo $FACET))
 ###
 ### Body (working with all facets exclude only facet named "ocean")
@@ -16,7 +29,10 @@ do
 		rm -rf client packager server zip/*.zip
 		### Copy project to workspace
 		if [ ! -d zip ]; then mkdir zip; fi
-		cp -Rf $CURRENT_BUILD/$GIT_COMMIT/* .
+		# this line commented because this job was moved to host dev02.design.isd.dp.ua
+		#cp -Rf $CURRENT_BUILD/$GIT_COMMIT/* .
+		# this line there because this job working in host dev02.design.isd.dp.ua
+		cp -Rf $CURRENT_BUILD/* .
 		### Create associative array
 		deploymentPackageId=($(echo $ID))
                 ELEMENT_OF_FACETS=($facet)
@@ -36,11 +52,21 @@ do
 			echo $i --- ${combineArray[$i]}
 			cd $WORKSPACE/packager
 			node index.js --target=macos --config=/home/jenkins/build_config --from=$WORKSPACE/client --manifest=$WORKSPACE/client/package.json --prefix=$BRANCH- --suffix=-$i --epubs=$CURRENT_EPUBS/$i
-					mv $WORKSPACE/packager/out/dest/*.zip $WORKSPACE/zip/
+			mv $WORKSPACE/packager/out/dest/*.zip $WORKSPACE/zip/
+			# this lines commented because this job was moved to host dev02.design.isd.dp.ua
+			#if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts ]; then
+			#	mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts
+			#fi
+			# this lines there because this job working in host dev02.design.isd.dp.ua
+			ssh jenkins@dev01.isd.dp.ua "
 			if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts ]; then
 				mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts
 			fi
-			cp $WORKSPACE/zip/*$i-macos*.zip $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts/
+			"
+			# this line commented because this job was moved to host dev02.design.isd.dp.ua
+			#cp $WORKSPACE/zip/*$i-macos*.zip $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts/
+			# this line there because this job working in host dev02.design.isd.dp.ua
+			scp $WORKSPACE/zip/*$i-macos*.zip jenkins@dev01.isd.dp.ua:$ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts/
 		done
 	else
 		printf "we can only work with the all facets exclude 'ocean' \n"
