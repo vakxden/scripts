10aeb14834508a069bb4f9007e719182998e5b3c
Denys Vachov <dvac@isd.dp.ua>
2014-05-27 13:10:19 +0300

    corrected irls-reader-initiate-nw-win.sh: if array FACET contain more than one

--- jenkins-irls/irls-reader-initiate-nw-win.sh
+++ jenkins-irls/irls-reader-initiate-nw-win.sh
@@ -9,52 +9,56 @@ export PATH=$PATH:$NODE_HOME/bin/
 BRANCH=$(echo $BRANCHNAME | sed 's/\//-/g' | sed 's/_/-/g')
 ARTIFACTS_DIR=/home/jenkins/irls-reader-artifacts
 CURRENT_EPUBS=$HOME/irls-reader-current-epubs
-#FACETS=(puddle bahaiebooks lake audio mediaoverlay)
 FACETS=($(echo $FACET))
 ###
 ### Body (working with all facets exclude only facet named "ocean")
 ###
-if [ "$FACETS" = "ocean" ]; then
-	printf "facet named 'ocean' will not be processed \n"
-else
-	### Remove old version of project and zip-archives
-	if [ ! -d zip ]; then mkdir zip; fi
-	### Copy project to workspace
-	# this line commented because this job was moved to host dev02.design.isd.dp.ua
-	#cp -Rf $CURRENT_BUILD/$GIT_COMMIT/* .
-	# this line there because this job working in host dev02.design.isd.dp.ua
-	cp -Rf $CURRENT_BUILD/* .
-	### Create associative array
-	deploymentPackageId=($(echo $ID))
-	declare -A combineArray
-	for ((i=0; i<${#deploymentPackageId[@]}; i++))
-	do
-		for ((y=0; y<${#FACETS[@]}; y++))
-		do
-			if [ -n "$(echo "${deploymentPackageId[i]}" | grep "${FACETS[y]}$")" ]; then
-				combineArray+=(["${FACETS[y]}"]="${deploymentPackageId[i]}")
-			fi
-		done
-	done
-	### Create zip-archive with application version for Windows
-	for i in "${!combineArray[@]}"
-	do
-		echo $i --- ${combineArray[$i]}
-		cd $WORKSPACE/packager
-		node index.js --target=win --config=/home/jenkins/build_config --from=$WORKSPACE/client --manifest=$WORKSPACE/client/package.json --prefix=$BRANCH- --suffix=-$i --epubs=$CURRENT_EPUBS/$i
-		mv $WORKSPACE/packager/out/dest/*.zip $WORKSPACE/zip/
-		# this lines commented because this job was moved to host dev02.design.isd.dp.ua
-		#if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts ]; then
-		#	mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts
-		#fi
-		ssh jenkins@dev01.isd.dp.ua "
-		if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts ]; then
-			mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts
-		fi
-		"
+
+for facet in ${FACETS[@]}
+do
+	if [ $(echo "$facet" | egrep "puddle|bahaiebooks|mediaoverlay|audio|audiobywords|lake") ]; then
+		### Remove old version of project and zip-archives
+		if [ ! -d zip ]; then mkdir zip; fi
+		### Copy project to workspace
 		# this line commented because this job was moved to host dev02.design.isd.dp.ua
-		#cp $WORKSPACE/zip/*$i-win*.zip $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts/
+		#cp -Rf $CURRENT_BUILD/$GIT_COMMIT/* .
 		# this line there because this job working in host dev02.design.isd.dp.ua
-		scp $WORKSPACE/zip/*$i-win*.zip jenkins@dev01.isd.dp.ua:$ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts/
-	done
-fi
+		cp -Rf $CURRENT_BUILD/* .
+		### Create associative array
+		deploymentPackageId=($(echo $ID))
+		ELEMENT_OF_FACETS=($facet)
+		declare -A combineArray
+		for ((x=0; x<${#deploymentPackageId[@]}; x++))
+		do
+			for ((y=0; y<${#ELEMENT_OF_FACETS[@]}; y++))
+			do
+				if [ -n "$(echo "${deploymentPackageId[x]}" | grep "${ELEMENT_OF_FACETS[y]}$")" ]; then
+					combineArray+=(["${ELEMENT_OF_FACETS[y]}"]="${deploymentPackageId[x]}")
+				fi
+			done
+		done
+		### Create zip-archive with application version for Windows
+		for i in "${!combineArray[@]}"
+		do
+			echo $i --- ${combineArray[$i]}
+			cd $WORKSPACE/packager
+			node index.js --target=win --config=/home/jenkins/build_config --from=$WORKSPACE/client --manifest=$WORKSPACE/client/package.json --prefix=$BRANCH- --suffix=-$i --epubs=$CURRENT_EPUBS/$i
+			mv $WORKSPACE/packager/out/dest/*.zip $WORKSPACE/zip/
+			# this lines commented because this job was moved to host dev02.design.isd.dp.ua
+			#if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts ]; then
+			#	mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts
+			#fi
+			ssh jenkins@dev01.isd.dp.ua "
+			if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts ]; then
+				mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts
+			fi
+			"
+			# this line commented because this job was moved to host dev02.design.isd.dp.ua
+			#cp $WORKSPACE/zip/*$i-win*.zip $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts/
+			# this line there because this job working in host dev02.design.isd.dp.ua
+			scp $WORKSPACE/zip/*$i-win*.zip jenkins@dev01.isd.dp.ua:$ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts/
+		done
+	else
+		printf "we can only work with the all facets exclude 'ocean' \n"
+	fi
+done
