c2aefcb97d5b13c0a95d1d30f73343a1bdd2ee7e
Denys Vachov <dvac@isd.dp.ua>
2014-07-10 17:12:10 +0300

    fix wrong loop in irls-reader-initiate-nw-macos.sh

--- jenkins-irls/irls-reader-initiate-nw-macos.sh
+++ jenkins-irls/irls-reader-initiate-nw-macos.sh
@@ -19,13 +19,31 @@ BRANCH=$(echo $BRANCHNAME | sed 's/\//-/g' | sed 's/_/-/g')
 ARTIFACTS_DIR=/home/jenkins/irls-reader-artifacts
 CURRENT_EPUBS=$HOME/irls-reader-current-epubs
 FACETS=($(echo $FACET))
+### Create associative array
+deploymentPackageId=($(echo $ID))
+declare -A combineArray
+for ((x=0; x<${#deploymentPackageId[@]}; x++))
+do
+	for ((y=0; y<${#FACETS[@]}; y++))
+	do
+		if [ -n "$(echo "${deploymentPackageId[x]}" | grep "${FACETS[y]}$")" ]; then
+			combineArray+=(["${FACETS[y]}"]="${deploymentPackageId[x]}")
+		fi
+	done
+done
 ###
 ### Body (working with all facets exclude only facet named "ocean")
 ###
-for facet in ${FACETS[@]}
+for i in "${!combineArray[@]}"
 do
-        if [ $(echo "$facet" | egrep "ocean$") ]; then
-		printf "we can only work with the all facets exclude 'ocean' \n"
+	echo $i --- ${combineArray[$i]}
+	if [ $(echo "$i" | egrep "ocean$") ]; then
+		getAbort()
+		{
+                	printf "we do not create the zip-file for facet named 'ocean'\n"
+		}
+		getAbort
+		trap 'getAbort; exit' SIGTERM
 	else
 		### Remove old version of project and zip-archives
 		rm -rf client packager server zip/*.zip
@@ -35,41 +53,24 @@ do
 		#cp -Rf $CURRENT_BUILD/$GIT_COMMIT/* .
 		# this line there because this job working in host dev02.design.isd.dp.ua
 		cp -Rf $CURRENT_BUILD/* .
-		### Create associative array
-		deploymentPackageId=($(echo $ID))
-                ELEMENT_OF_FACETS=($facet)
-                declare -A combineArray
-                for ((x=0; x<${#deploymentPackageId[@]}; x++))
-                do
-                        for ((y=0; y<${#ELEMENT_OF_FACETS[@]}; y++))
-                        do
-                                if [ -n "$(echo "${deploymentPackageId[x]}" | grep "${ELEMENT_OF_FACETS[y]}$")" ]; then
-                                        combineArray+=(["${ELEMENT_OF_FACETS[y]}"]="${deploymentPackageId[x]}")
-                                fi
-                        done
-                done
 		### Create zip-archive with application version for MacOS
-		for i in "${!combineArray[@]}"
-		do
-			echo $i --- ${combineArray[$i]}
-			cd $WORKSPACE/packager
-			node index.js --target=macos --config=/home/jenkins/build_config --from=$WORKSPACE/client --manifest=$WORKSPACE/client/package.json --prefix=$BRANCH- --suffix=-$i --epubs=$CURRENT_EPUBS/$i
-			mv $WORKSPACE/packager/out/dest/*.zip $WORKSPACE/zip/
-			# this lines commented because this job was moved to host dev02.design.isd.dp.ua
-			#if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts ]; then
-			#	mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts
-			#fi
-			# this lines there because this job working in host dev02.design.isd.dp.ua
-			ssh jenkins@dev01.isd.dp.ua "
-			if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts ]; then
-				mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts
-			fi
-			"
-			# this line commented because this job was moved to host dev02.design.isd.dp.ua
-			#cp $WORKSPACE/zip/*$i-macos*.zip $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts/
-			# this line there because this job working in host dev02.design.isd.dp.ua
-			ssh jenkins@dev01.isd.dp.ua " rm -rf /var/lib/jenkins/jobs/irls-reader-initiate-nw-macos/builds/lastSuccessfulBuild/archive/zip/*.zip"
-			scp $WORKSPACE/zip/*$i-macos*.zip jenkins@dev01.isd.dp.ua:$ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts/
-		done
+		cd $WORKSPACE/packager
+		node index.js --target=macos --config=/home/jenkins/build_config --from=$WORKSPACE/client --manifest=$WORKSPACE/client/package.json --prefix=$BRANCH- --suffix=-$i --epubs=$CURRENT_EPUBS/$i
+		mv $WORKSPACE/packager/out/dest/*.zip $WORKSPACE/zip/
+		# this lines commented because this job was moved to host dev02.design.isd.dp.ua
+		#if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts ]; then
+		#	mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts
+		#fi
+		# this lines there because this job working in host dev02.design.isd.dp.ua
+		ssh jenkins@dev01.isd.dp.ua "
+		if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts ]; then
+			mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts
+		fi
+		"
+		# this line commented because this job was moved to host dev02.design.isd.dp.ua
+		#cp $WORKSPACE/zip/*$i-macos*.zip $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts/
+		# this line there because this job working in host dev02.design.isd.dp.ua
+		ssh jenkins@dev01.isd.dp.ua " rm -rf /var/lib/jenkins/jobs/irls-reader-initiate-nw-macos/builds/lastSuccessfulBuild/archive/zip/*.zip"
+		scp $WORKSPACE/zip/*$i-macos*.zip jenkins@dev01.isd.dp.ua:$ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts/
 	fi
 done
