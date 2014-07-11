48cfeadaee5d451e121bb3b68e9aedf71ee5ea83
Denys Vachov <dvac@isd.dp.ua>
2014-07-11 11:10:04 +0300

    remove archiving artifacts functional in irls-reader-initiate-nw-win.sh

--- jenkins-irls/irls-reader-initiate-nw-win.sh
+++ jenkins-irls/irls-reader-initiate-nw-win.sh
@@ -43,40 +43,23 @@ do
 		#cp -Rf $CURRENT_BUILD/$GIT_COMMIT/* .
 		# this line there because this job working in host dev02.design.isd.dp.ua
 		cp -Rf $CURRENT_BUILD/* .
-		### Create associative array
-		deploymentPackageId=($(echo $ID))
-		ELEMENT_OF_FACETS=($facet)
-		declare -A combineArray
-		for ((x=0; x<${#deploymentPackageId[@]}; x++))
-		do
-			for ((y=0; y<${#ELEMENT_OF_FACETS[@]}; y++))
-			do
-				if [ -n "$(echo "${deploymentPackageId[x]}" | grep "${ELEMENT_OF_FACETS[y]}$")" ]; then
-					combineArray+=(["${ELEMENT_OF_FACETS[y]}"]="${deploymentPackageId[x]}")
-				fi
-			done
-		done
 		### Create zip-archive with application version for Windows
-		for i in "${!combineArray[@]}"
-		do
-			echo $i --- ${combineArray[$i]}
-			cd $WORKSPACE/packager
-			node index.js --target=win --config=/home/jenkins/build_config --from=$WORKSPACE/client --manifest=$WORKSPACE/client/package.json --prefix=$BRANCH- --suffix=-$i --epubs=$CURRENT_EPUBS/$i
-			mv $WORKSPACE/packager/out/dest/*.zip $WORKSPACE/zip/
-			# this lines commented because this job was moved to host dev02.design.isd.dp.ua
-			#if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts ]; then
-			#	mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts
-			#fi
-			ssh jenkins@dev01.isd.dp.ua "
-			if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts ]; then
-				mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts
-			fi
-			"
-			# this line commented because this job was moved to host dev02.design.isd.dp.ua
-			#cp $WORKSPACE/zip/*$i-win*.zip $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts/
-			# this line there because this job working in host dev02.design.isd.dp.ua
-			ssh jenkins@dev01.isd.dp.ua " rm -rf /var/lib/jenkins/jobs/irls-reader-initiate-nw-win/lastSuccessful/archive/zip/*.zip"
-			scp $WORKSPACE/zip/*$i-win*.zip jenkins@dev01.isd.dp.ua:$ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts/
-		done
+		cd $WORKSPACE/packager
+		node index.js --target=win --config=/home/jenkins/build_config --from=$WORKSPACE/client --manifest=$WORKSPACE/client/package.json --prefix=$BRANCH- --suffix=-$i --epubs=$CURRENT_EPUBS/$i
+		mv $WORKSPACE/packager/out/dest/*.zip $WORKSPACE/zip/
+		# this lines commented because this job was moved to host dev02.design.isd.dp.ua
+		#if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts ]; then
+		#	mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts
+		#fi
+		ssh jenkins@dev01.isd.dp.ua "
+		if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts ]; then
+			mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts
+		fi
+		"
+		# this line commented because this job was moved to host dev02.design.isd.dp.ua
+		#cp $WORKSPACE/zip/*$i-win*.zip $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts/
+		# this line there because this job working in host dev02.design.isd.dp.ua
+		scp $WORKSPACE/zip/*$i-win*.zip jenkins@dev01.isd.dp.ua:$ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts/
+		rm -rf $WORKSPACE/zip
 	fi
 done
