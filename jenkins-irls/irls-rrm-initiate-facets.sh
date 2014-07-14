0d9ee3764eff2a26429ab05fe31a5a67e50199ce
Denys Vachov <dvac@isd.dp.ua>
2014-07-14 14:06:21 +0300

    add irls-rrm-initiate-facets.sh

---
+++ jenkins-irls/irls-rrm-initiate-facets.sh
@@ -0,0 +1,16 @@
+if [ "$BRANCHNAME" = "master" ]; then
+	#FACET=(puddle farsi farsiref bahaiebooks audio audiobywords mediaoverlay lake ocean)
+	FACET=(farsi audio)
+	for i in ${FACET[@]}
+	do
+		curl http://wpp.isd.dp.ua/jenkins/view/irls-rrm-processor/job/irls-rrm-processor-convert/buildWithParameters?token=Sheedah8\&FACET=$i
+	done
+elif [ "$BRANCHNAME" = "audio" ]; then
+	FACET=(audio)
+	for i in ${FACET[@]}
+	do
+		curl http://wpp.isd.dp.ua/jenkins/view/irls-rrm-processor/job/irls-rrm-processor-convert/buildWithParameters?token=Sheedah8\&FACET=$i
+	done
+else
+	exit 1
+fi
