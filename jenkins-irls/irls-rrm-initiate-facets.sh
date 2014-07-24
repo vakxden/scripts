74eea95a079ef16cbee34b26d7be77792dfe1368
Denys Vachov <dvac@isd.dp.ua>
2014-07-24 15:20:25 +0300

    disable conver for not master branch

--- jenkins-irls/irls-rrm-initiate-facets.sh
+++ jenkins-irls/irls-rrm-initiate-facets.sh
@@ -4,16 +4,4 @@ if [ "$BRANCHNAME" = "master" ]; then
 	do
 		curl http://wpp.isd.dp.ua/jenkins/view/irls-rrm-processor/job/irls-rrm-processor-convert/buildWithParameters?token=Sheedah8\&FACET=$i
 	done
-elif [ "$BRANCHNAME" = "audio" ]; then
-	FACET=(audio)
-	for i in ${FACET[@]}
-	do
-		curl http://wpp.isd.dp.ua/jenkins/view/irls-rrm-processor/job/irls-rrm-processor-convert/buildWithParameters?token=Sheedah8\&FACET=$i
-	done
-else
-	FACET=(puddle farsi bahaiebooks audio mediaoverlay lake)
-	for i in ${FACET[@]}
-	do
-		curl http://wpp.isd.dp.ua/jenkins/view/irls-rrm-processor/job/irls-rrm-processor-convert/buildWithParameters?token=Sheedah8\&FACET=$i
-	done
 fi
