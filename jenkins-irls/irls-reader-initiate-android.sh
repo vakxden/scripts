00db4266e140598a76cd275851e5ce3d741d55fd
Denys Vachov <dvac@isd.dp.ua>
2014-08-06 16:49:23 +0300

    fix funct

--- jenkins-irls/irls-reader-initiate-android.sh
+++ jenkins-irls/irls-reader-initiate-android.sh
@@ -1,22 +1,19 @@
 ###
 ### Variables
 ###
-
 export NO_PROXY="127.0.0.1, 10.98.244.26, localhost, *.loc"
 export FTP_PROXY="http://10.98.192.120:3128"
 export HTTPS_PROXY="http://10.98.192.120:3128"
 export https_proxy="http://10.98.192.120:3128"
 export HTTP_PROXY="http://10.98.192.120:3128"
 export http_proxy="http://10.98.192.120:3128"
-
-BRANCH=$(echo $BRANCHNAME | sed 's/\//-/g' | sed 's/_/-/g')
-ARTIFACTS_DIR=/home/jenkins/irls-reader-artifacts
-CURRENT_EPUBS=$HOME/irls-reader-current-epubs
 export NODE_HOME=/opt/node
 export ANDROID_HOME=/opt/android-sdk-linux
 export PATH=$PATH:$NODE_HOME/bin:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$JAVA_HOME/bin
+BRANCH=$(echo $BRANCHNAME | sed 's/\//-/g' | sed 's/_/-/g')
+ARTIFACTS_DIR=/home/jenkins/irls-reader-artifacts
+CURRENT_EPUBS=$HOME/irls-reader-current-epubs
 FACETS=($(echo $FACET))
-
 
 ### Create associative array
 deploymentPackageId=($(echo $ID))
@@ -31,14 +28,14 @@ do
 	done
 done
 
-
 ###
 ### Body (working with all facets exclude "ocean")
 ###
 
 ### Main loop
 function main_loop {
-	function sub_main_loop {
+	notmainloop ()
+	{
 	if [ $(echo "$i" | egrep "ocean$") ]; then
 		getAbort()
 		{
@@ -58,7 +55,8 @@ function main_loop {
 		fi
 		"
 		time scp $WORKSPACE/$BRANCH-FFA_Reader-$i.apk  jenkins@dev01.isd.dp.ua:$ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts/ && rm -f $WORKSPACE/$BRANCH-FFA_Reader-$i.apk
-        fi}
+        fi
+	}
 
 	for i in "${!combineArray[@]}"
 	do
@@ -72,13 +70,13 @@ function main_loop {
 		if [ "$BRANCHNAME" = "feature/platforms-config" ]; then
 			grep "platforms.*android" $WORKSPACE/targets/"$i"_"FFA"/targetConfig.json
 			if [ $? -eq 0 ]; then
-				sub_main_loop
+				notmainloop
 			else
 				echo "Shutdown of this job because platform \"android\" not found in config targetConfig.json"
 				exit 0
 			fi
 		else
-			sub_main_loop
+			notmainloop
 		fi
 	done
 }
