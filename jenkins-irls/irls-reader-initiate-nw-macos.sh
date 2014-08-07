59025a9b44c8ce51f000c671047146652f9ff57a
Denys Vachov <dvac@isd.dp.ua>
2014-08-07 12:03:34 +0300

    add checking platform for irls-reader-initiate-nw-macos

--- jenkins-irls/irls-reader-initiate-nw-macos.sh
+++ jenkins-irls/irls-reader-initiate-nw-macos.sh
@@ -38,7 +38,31 @@ done
 ###
 # Main loop
 function main_loop {
-
+	notmainloop ()
+	{
+	if [ $(echo "$i" | egrep "ocean$") ]; then
+		getAbort()
+		{
+                	printf "we do not create the zip-file for facet named 'ocean'\n"
+		}
+		getAbort
+		trap 'getAbort; exit' SIGTERM
+	else
+		### Copy project to workspace
+		if [ ! -d $WORKSPACE/zip ]; then mkdir $WORKSPACE/zip; fi
+		### Create zip-archive with application version for MacOS
+		cd $WORKSPACE/packager
+		time node index.js --platform=macos --config=$WORKSPACE/targets --from=$WORKSPACE/client --manifest=$WORKSPACE/client/package.json --prefix=$BRANCH- --epubs=$CURRENT_EPUBS
+		mv $WORKSPACE/packager/out/dest/*.zip $WORKSPACE/zip/
+		ssh jenkins@dev01.isd.dp.ua "
+		if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts ]; then
+			mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts
+		fi
+		"
+		time scp $WORKSPACE/zip/*$i-macos*.zip jenkins@dev01.isd.dp.ua:$ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts/ && rm -f $WORKSPACE/zip/*$i-macos*.zip
+		rm -rf $WORKSPACE/zip
+	fi
+	}
 	for i in "${!combineArray[@]}"
 	do
 		rm -rf $WORKSPACE/*
@@ -46,39 +70,16 @@ function main_loop {
 		cp -Rf $CURRENT_BUILD/$GIT_COMMIT_TARGET/* $WORKSPACE/
 
 		echo $i --- ${combineArray[$i]}
-		if [ $(echo "$i" | egrep "ocean$") ]; then
-			getAbort()
-			{
-	                	printf "we do not create the zip-file for facet named 'ocean'\n"
-			}
-			getAbort
-			trap 'getAbort; exit' SIGTERM
-		else
-			### Copy project to workspace
-			if [ ! -d $WORKSPACE/zip ]; then mkdir $WORKSPACE/zip; fi
-			### Create zip-archive with application version for MacOS
-			cd $WORKSPACE/packager
-			#if [ "$BRANCHNAME" = "feature/target" ]; then
-			time node index.js --platform=macos --config=$WORKSPACE/targets --from=$WORKSPACE/client --manifest=$WORKSPACE/client/package.json --prefix=$BRANCH- --epubs=$CURRENT_EPUBS
-			#else
-			#	time node index.js --target=macos --config=/home/jenkins/build_config --from=$WORKSPACE/client --manifest=$WORKSPACE/client/package.json --prefix=$BRANCH- --suffix=-$i --epubs=$CURRENT_EPUBS/$i
-			#fi
-			mv $WORKSPACE/packager/out/dest/*.zip $WORKSPACE/zip/
-			# this lines commented because this job was moved to host dev02.design.isd.dp.ua
-			#if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts ]; then
-			#	mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts
-			#fi
-			# this lines there because this job working in host dev02.design.isd.dp.ua
-			ssh jenkins@dev01.isd.dp.ua "
-			if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts ]; then
-				mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts
+		### Checking
+		if [ "$BRANCHNAME" = "feature/platforms-config" ]; then
+			if grep "platforms.*macos" $WORKSPACE/targets/"$i"_"FFA"/targetConfig.json; then
+				notmainloop
+			else
+				echo "Shutdown of this job because platform \"macos\" not found in config targetConfig.json"
+				exit 0
 			fi
-			"
-			# this line commented because this job was moved to host dev02.design.isd.dp.ua
-			#cp $WORKSPACE/zip/*$i-macos*.zip $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts/
-			# this line there because this job working in host dev02.design.isd.dp.ua
-			time scp $WORKSPACE/zip/*$i-macos*.zip jenkins@dev01.isd.dp.ua:$ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts/ && rm -f $WORKSPACE/zip/*$i-macos*.zip
-			rm -rf $WORKSPACE/zip
+		else
+			notmainloop
 		fi
 	done
 }
