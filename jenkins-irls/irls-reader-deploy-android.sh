9bcc5bc95ebca085d744ba58d71d7ca3b814a030
Denys Vachov <dvac@isd.dp.ua>
2014-08-22 10:38:14 +0300

    fix description RegEx

--- jenkins-irls/irls-reader-deploy-android.sh
+++ jenkins-irls/irls-reader-deploy-android.sh
@@ -5,16 +5,17 @@ if [ -z $BRANCHNAME ]; then
 	printf "[ERROR_BRANCHNAME] Branchname must be passed \n"
 	exit 1
 fi
+
 if [ -z $mark ]; then
 	printf "[ERROR_MARK] mark must be passed \n"
 	exit 1
 elif [ "$mark" = "all" ] || [ "$mark" = "initiate-android" ]; then
-	printf "[WARN_MARK] mark is \"all\" or \"initiate-android\" \n"
+	echo "[WARN_MARK] branch name is <b>$BRANCHNAME</b><br>dest is <b>$dest</b><br>ID is <b>$ID</b>"
 elif [ "$mark" ! = "all" ] || [ "$mark" ! = "initiate-android" ]; then
 	printf "[ERROR_MARK] mark must be \"all\" or \"initiate-android\"! Not \"$mark\"! \n"
-	#mark is <b>$mark</b><br>branch name is <b>$BRANCHNAME</b><br>dest is <b>$dest</b> <br> and ID== <b> $ID</b>
 	exit 1
 fi
+
 ###
 ### Constant local variables
 ###
@@ -23,6 +24,7 @@ BUILD_ID=donotkillme
 CURRENT_ART_PATH=/home/jenkins/irls-reader-artifacts
 STAGE_ART_PATH=/home/jenkins/irls-reader-artifacts-stage
 FACETS=($(echo $FACET))
+
 ###
 ### Create associative array
 ###
@@ -38,6 +40,7 @@ do
                 fi
         done
 done
+
 ###
 ### Functions
 ###
@@ -64,6 +67,7 @@ function search_and_copy {
                 fi
         fi
 }
+
 function generate_files {
         cd $1
         sudo /home/jenkins/scripts/portgenerator-for-deploy.sh $BRANCH $i $dest ${combineArray[$i]}
@@ -71,6 +75,7 @@ function generate_files {
         ls -lah
         echo PWD=$PWD
 }
+
 function pid_node {
         # $1 = $2 (server/$INDEX_FILE) from function start_node = $INDEX_FILE
         ### Starting (or restarting) node server
@@ -83,6 +88,7 @@ function pid_node {
                 fi
                 rm -f local.json irls-current-reader-* irls-stage-reader-*
 }
+
 function start_node {
         # if content for running nodejs-server exists?
         # $1=$PKG_DIR
@@ -104,95 +110,59 @@ function start_node {
                 fi
         fi
 }
+
 ###
-### If the variable $mark is equal to the value of "all" or "initiate-nw-win", then perform the body of this script
+### Body
 ###
-#if [ "$mark" = "all" ] || [ "$mark" = "initiate-android" ]; then
-        ###
-        ### Body
-        ###
-        if [ "$dest" = "DEVELOPMENT" ]; then
-                for i in "${!combineArray[@]}"
-                do
-                        # variables
-                        ARTIFACTS_DIR=$CURRENT_ART_PATH/${combineArray[$i]}/packages/artifacts
-                        PKG_DIR=$CURRENT_ART_PATH/${combineArray[$i]}/packages
-                        INDEX_FILE='index_'$i'_'$BRANCH'.js'
-                        # output value for a pair "key-value"
-                        echo $i --- ${combineArray[$i]}
-                        # checking the existence of a directory with the artifacts
-                        if [ ! -d $ARTIFACTS_DIR ]; then
-                                mkdir -p $ARTIFACTS_DIR
-                        fi
-                        # search apk-files, if not exists - copy to artifacts dir
-                        search_and_copy $ARTIFACTS_DIR/
-                        # generate index.html and local.json
-                        generate_files $PKG_DIR
-                        # run (re-run) node
-                        start_node $PKG_DIR $INDEX_FILE
-                        # update environment.json file
-                        /home/jenkins/scripts/search_for_environment.sh "${combineArray[$i]}" "$dest"
-                done
-        elif [ "$dest" = "STAGE" ]; then
-                for i in "${!combineArray[@]}"
-                do
-                        # variables
-                        CURRENT_ARTIFACTS_DIR=$CURRENT_ART_PATH/${combineArray[$i]}/packages/artifacts
-                        STAGE_ARTIFACTS_DIR=$STAGE_ART_PATH/${combineArray[$i]}/packages/artifacts
-                        PKG_DIR=$STAGE_ART_PATH/${combineArray[$i]}/packages
-                        INDEX_FILE='index_'$i'_'$BRANCH'_'$dest'.js'
-                        # output value for a pair "key-value"
-                        echo $i --- ${combineArray[$i]}
-                        # checking the existence of a directory with the artifacts
-                        if [ ! -d $STAGE_ARTIFACTS_DIR ]; then
-                                mkdir -p $STAGE_ARTIFACTS_DIR
-                        fi
-                        cd $STAGE_ARTIFACTS_DIR
-                        # search apk-files, if not exists - copy from artifacts dir to stage artifacts dir
-                        search_and_copy $STAGE_ARTIFACTS_DIR/ $CURRENT_ARTIFACTS_DIR/
-                        # generate index.html and local.json
-                        generate_files $PKG_DIR
-                        # run (re-run) node
-                        start_node $PKG_DIR $INDEX_FILE
-                        # update environment.json file
-                        /home/jenkins/scripts/search_for_environment.sh "${combineArray[$i]}" "$dest"
-                done
-        elif [ "$dest" = "LIVE" ]; then
-                exit 0
-#               for i in "${!combineArray[@]}"
-#               do
-#                       # output value for a pair "key-value"
-#                       echo $i --- ${combineArray[$i]}
-#                       ssh dvac@devzone.dp.ua "
-#                               if [ ! -d  ~/irls-reader-artifacts/${combineArray[$i]}/packages/art ]
-#                               then
-#                                       mkdir -p ~/irls-reader-artifacts/${combineArray[$i]}/packages/art
-#                               else
-#                                       rm -rf  ~/irls-reader-artifacts/${combineArray[$i]}/packages/art/*$i*.apk
-#                               fi"
-#                       ARTIFACTS_DIR=$STAGE_ART_PATH/${combineArray[$i]}/packages/artifacts
-#                       if [ -f $ARTIFACTS_DIR/*$i*.apk ]; then
-#                               scp $ARTIFACTS_DIR/*$i*.apk dvac@devzone.dp.ua:~/irls-reader-artifacts/${combineArray[$i]}/packages/art/
-#                       fi
-#                       ssh dvac@devzone.dp.ua "
-#                               /home/dvac/scripts/portgen-deploy-live.sh $BRANCH $i $dest ${combineArray[$i]}
-#                               cp ~/local.json ~/irls-reader-artifacts/${combineArray[$i]}/packages/server/config
-#                               INDEX_FILE=index_"$i"_$BRANCH.js
-#                               cd ~/irls-reader-artifacts/${combineArray[$i]}/packages/
-#                               PID=\$(ps aux | grep node.*server/\$INDEX_FILE | grep -v grep | /usr/bin/awk '{print \$2}')
-#                               if [ ! -z \$PID ]
-#                               then
-#                                       kill -9 \$PID
-#                                       nohup ~/node/bin/node server/\$INDEX_FILE > /dev/null 2>&1 &
-#                               else
-#                                       nohup ~/node/bin/node server/\$INDEX_FILE > /dev/null 2>&1 &
-#                               fi"
-#                       echo link-$i-$dest="http://irls.websolutions.dp.ua/$i/$BRANCH/client/dist/app/index.html" >> $WORKSPACE/myenv
-#                       # update environment.json file
-#                       /home/jenkins/scripts/search_for_environment.sh "${combineArray[$i]}" "$dest"
-#               done
-        else
-                printf "[ERROR_DEST] dest must be DEVELOPMENT or STAGE or LIVE! Not $dest! \n"
-                exit 1
-        fi
-#fi
+if [ "$dest" = "DEVELOPMENT" ]; then
+        for i in "${!combineArray[@]}"
+        do
+                # variables
+                ARTIFACTS_DIR=$CURRENT_ART_PATH/${combineArray[$i]}/packages/artifacts
+                PKG_DIR=$CURRENT_ART_PATH/${combineArray[$i]}/packages
+                INDEX_FILE='index_'$i'_'$BRANCH'.js'
+                # output value for a pair "key-value"
+                echo $i --- ${combineArray[$i]}
+                # checking the existence of a directory with the artifacts
+                if [ ! -d $ARTIFACTS_DIR ]; then
+                        mkdir -p $ARTIFACTS_DIR
+                fi
+                # search apk-files, if not exists - copy to artifacts dir
+                search_and_copy $ARTIFACTS_DIR/
+                # generate index.html and local.json
+                generate_files $PKG_DIR
+                # run (re-run) node
+                start_node $PKG_DIR $INDEX_FILE
+                # update environment.json file
+                /home/jenkins/scripts/search_for_environment.sh "${combineArray[$i]}" "$dest"
+        done
+elif [ "$dest" = "STAGE" ]; then
+        for i in "${!combineArray[@]}"
+        do
+                # variables
+                CURRENT_ARTIFACTS_DIR=$CURRENT_ART_PATH/${combineArray[$i]}/packages/artifacts
+                STAGE_ARTIFACTS_DIR=$STAGE_ART_PATH/${combineArray[$i]}/packages/artifacts
+                PKG_DIR=$STAGE_ART_PATH/${combineArray[$i]}/packages
+                INDEX_FILE='index_'$i'_'$BRANCH'_'$dest'.js'
+                # output value for a pair "key-value"
+                echo $i --- ${combineArray[$i]}
+                # checking the existence of a directory with the artifacts
+                if [ ! -d $STAGE_ARTIFACTS_DIR ]; then
+                        mkdir -p $STAGE_ARTIFACTS_DIR
+                fi
+                cd $STAGE_ARTIFACTS_DIR
+                # search apk-files, if not exists - copy from artifacts dir to stage artifacts dir
+                search_and_copy $STAGE_ARTIFACTS_DIR/ $CURRENT_ARTIFACTS_DIR/
+                # generate index.html and local.json
+                generate_files $PKG_DIR
+                # run (re-run) node
+                start_node $PKG_DIR $INDEX_FILE
+                # update environment.json file
+                /home/jenkins/scripts/search_for_environment.sh "${combineArray[$i]}" "$dest"
+        done
+elif [ "$dest" = "LIVE" ]; then
+        exit 0
+else
+        printf "[ERROR_DEST] dest must be DEVELOPMENT or STAGE or LIVE! Not $dest! \n"
+        exit 1
+fi
