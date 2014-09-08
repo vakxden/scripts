c11f7117f8f90aae50fb5ae8cbc9c3592f5609b3
Denys Vachov <dvac@isd.dp.ua>
2014-09-08 14:19:26 +0300

    delete old lines

--- jenkins-irls/irls-rrm-processor-convert.sh
+++ jenkins-irls/irls-rrm-processor-convert.sh
@@ -4,7 +4,6 @@ CURRENT_TEXTS=$HOME/irls-reader-current-texts
 PROJECTNAME=$(basename $CURRENT_RRM)
 RESULTS=$WORKSPACE/results
 CURRENT_EPUBS=$HOME/irls-reader-current-epubs
-#FACETS=(puddle lake bahaiebooks ocean audio mediaoverlay)
 FACETS=($(echo $FACET))
 META1=$HOME/irls-rrm-processor-deploy/meta-processor-deploy
 META2=$HOME/irls-reader-current-texts/meta-ocean-deploy
@@ -17,9 +16,6 @@ cd $WORKSPACE/$PROJECTNAME
 
 N=$(echo $BUILD_DISPLAY_NAME | sed 's/\#//g')
 cat /dev/null > $WORKSPACE/filesconv.txt
-
-### Before starting epubchecker (!)
-#rm -f $WORKSPACE/epubcheck*.log
 
 ###
 ### Running convert for all facets
@@ -37,13 +33,6 @@ do
 	cat $META1 >> $CURRENT_EPUBS/$i/$META_SUM && cat $META2 >> $CURRENT_EPUBS/$i/$META_SUM
 	# echo numbers of converted files to temporary file
 	files_conv=$(grep "Files converted.*$i" /var/lib/jenkins/jobs/irls-rrm-processor-convert/builds/$N/log | grep -v grep >> $WORKSPACE/filesconv.txt)
-	### Starting epubchecker
-	#cd $WORKSPACE
-	#/opt/epubcheck.sh $CURRENT_EPUBS/$i/
-	# for set description (link to graph for ocean)
-	if [ "$i" = "ocean" ]; then
-		printf "WARN_OCEAN \n"
-	fi
 done
 
 cat /dev/null > $CURRENT_EPUBS/$META_SUM_ALL
@@ -56,8 +45,6 @@ DATE=$(date +%s)
 files_conv_ocean=$(grep ocean $WORKSPACE/filesconv.txt | awk '{print $6}')
 if [ ! -z "$files_conv_ocean" ]; then
 	zabbix_sender -z 127.0.0.1 p 10051 -s "dev01" -k files_conv_ocean -o "$files_conv_ocean"
-	#sudo rrdtool update /var/db/rrdtool/trendcountbooks.rrd $DATE:$files_conv_ocean
-	#sudo rrdtool graph /home/jenkins/irls-reader-artifacts/trendcountbooks.png -a PNG -v "irls-rrm-processor-convert" --start now-14d --end N -w 1200 -h 400 DEF:numbers_of=/var/db/rrdtool/trendcountbooks.rrd:numbers_of:LAST AREA:numbers_of#00FF00:"Numbers of files converted"
 fi
 
 ###
