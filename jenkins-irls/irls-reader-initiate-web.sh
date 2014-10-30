cbbe23c3fa442893c278d870f3da21a146ba1557
Denys Vachov <dvac@isd.dp.ua>
2014-10-30 11:57:52 +0200

    delete old commented lines

--- jenkins-irls/irls-reader-initiate-web.sh
+++ jenkins-irls/irls-reader-initiate-web.sh
@@ -13,12 +13,6 @@ for ((i=0; i<${#deploymentPackageId[@]}; i++))
 do
 	a=$(echo "${deploymentPackageId[i]}"| cut -d"_" -f 2-)
 	combineArray+=(["$a"]="${deploymentPackageId[i]}")
-        #for ((y=0; y<${#TARGET[@]}; y++))
-        #do
-        #        if [ -n "$(echo "${deploymentPackageId[i]}" | grep "${TARGET[y]}$")" ]; then
-        #                combineArray+=(["${TARGET[y]}"]="${deploymentPackageId[i]}")
-        #        fi
-        #done
 done
 ### Create web-version of application
 function main_loop {
