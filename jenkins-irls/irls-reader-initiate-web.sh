0c1426fc4285330ab6c335779a67e7e74c56583a
Denys Vachov <dvac@isd.dp.ua>
2014-10-30 12:01:40 +0200

    commented initDB feature

--- jenkins-irls/irls-reader-initiate-web.sh
+++ jenkins-irls/irls-reader-initiate-web.sh
@@ -24,52 +24,54 @@ function main_loop {
                 cd $WORKSPACE
                 sudo $SCRIPTS_DIR/portgenerator-for-convert.sh $i
                 cp local.json $WORKSPACE/server/config && rm -f local.json
-                cd $WORKSPACE/server
-                time node initDB.js
+		### old feature below commented
+                #cd $WORKSPACE/server
+                #time node initDB.js
                 if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages ]; then
                         mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages
                 fi
                 if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages/client ]; then
                         mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages/client
                 fi
-                if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes ]; then
-                        mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes
-                fi
+                #if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes ]; then
+                #        mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes
+                #fi
                 cp -Rf $WORKSPACE/common $ARTIFACTS_DIR/${combineArray[$i]}/packages/
                 cp -Rf $WORKSPACE/server $ARTIFACTS_DIR/${combineArray[$i]}/packages/
                 cp -Rf $WORKSPACE/portal $ARTIFACTS_DIR/${combineArray[$i]}/packages/
                 cp -Rf $WORKSPACE/books $ARTIFACTS_DIR/${combineArray[$i]}/packages/
                 cp -Rf $WORKSPACE/packager/out/dest/*/* $ARTIFACTS_DIR/${combineArray[$i]}/packages/client
-                ls -l /home/couchdb/"$i"_*.couch
+                #ls -l /home/couchdb/"$i"_*.couch
                 #sudo service couchdb restart
-                ls -l /home/couchdb/"$i"_*.couch
-                sleep 15
-                ls -l /home/couchdb/"$i"_*.couch
-                cp -f /home/couchdb/"$i"_books.couch $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes/ && ls -l $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes/
-                cp -f /home/couchdb/"$i"_sentences.couch $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes/ && ls -l $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes/
-                cp -f /home/couchdb/"$i"_words.couch $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes/ && ls -l $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes/
-                ls -l $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes/
-                sleep 10
-                ls -l $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes/
-                MD5SUM_BOOKS_COUCH=$(md5sum /home/couchdb/"$i"_books.couch | awk '{print $1}')
-                MD5SUM_BOOKS_COPYED=$(md5sum $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes/"$i"_books.couch | awk '{print $1}')
-                if ! [ "$MD5SUM_BOOKS_COUCH" = "$MD5SUM_BOOKS_COPYED" ]; then
-                        echo "md5sum of copyed '$i'_books.couch not equal"
-                        exit 1
-                fi
-                MD5SUM_SENTENCES_COUCH=$(md5sum /home/couchdb/"$i"_sentences.couch | awk '{print $1}')
-                MD5SUM_SENTENCES_COPYED=$(md5sum $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes/"$i"_sentences.couch | awk '{print $1}')
-                if ! [ "$MD5SUM_SENTENCES_COUCH" = "$MD5SUM_SENTENCES_COPYED" ]; then
-                        echo "md5sum of copyed '$i'_sentences.couch not equal"
-                        exit 1
-                fi
-                MD5SUM_WORDS_COUCH=$(md5sum /home/couchdb/"$i"_words.couch | awk '{print $1}')
-                MD5SUM_WORDS_COPYED=$(md5sum $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes/"$i"_words.couch | awk '{print $1}')
-                if ! [ "$MD5SUM_WORDS_COUCH" = "$MD5SUM_WORDS_COPYED" ]; then
-                        echo "md5sum of copyed '$i'_words.couch not equal"
-                        echo "[ERROR_MD5] Error of md5sum!"
-                        exit 1
-                fi
+                #ls -l /home/couchdb/"$i"_*.couch
+                #sleep 15
+                #ls -l /home/couchdb/"$i"_*.couch
+                #cp -f /home/couchdb/"$i"_books.couch $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes/ && ls -l $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes/
+                #cp -f /home/couchdb/"$i"_sentences.couch $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes/ && ls -l $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes/
+                #cp -f /home/couchdb/"$i"_words.couch $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes/ && ls -l $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes/
+                #ls -l $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes/
+                #sleep 10
+                #ls -l $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes/
+                #MD5SUM_BOOKS_COUCH=$(md5sum /home/couchdb/"$i"_books.couch | awk '{print $1}')
+                #MD5SUM_BOOKS_COPYED=$(md5sum $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes/"$i"_books.couch | awk '{print $1}')
+                #if ! [ "$MD5SUM_BOOKS_COUCH" = "$MD5SUM_BOOKS_COPYED" ]; then
+                #        echo "md5sum of copyed '$i'_books.couch not equal"
+                #        exit 1
+                #fi
+                #MD5SUM_SENTENCES_COUCH=$(md5sum /home/couchdb/"$i"_sentences.couch | awk '{print $1}')
+                #MD5SUM_SENTENCES_COPYED=$(md5sum $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes/"$i"_sentences.couch | awk '{print $1}')
+                #if ! [ "$MD5SUM_SENTENCES_COUCH" = "$MD5SUM_SENTENCES_COPYED" ]; then
+                #        echo "md5sum of copyed '$i'_sentences.couch not equal"
+                #        exit 1
+                #fi
+                #MD5SUM_WORDS_COUCH=$(md5sum /home/couchdb/"$i"_words.couch | awk '{print $1}')
+                #MD5SUM_WORDS_COPYED=$(md5sum $ARTIFACTS_DIR/${combineArray[$i]}/packages/couchdb_indexes/"$i"_words.couch | awk '{print $1}')
+                #if ! [ "$MD5SUM_WORDS_COUCH" = "$MD5SUM_WORDS_COPYED" ]; then
+                #        echo "md5sum of copyed '$i'_words.couch not equal"
+                #        echo "[ERROR_MD5] Error of md5sum!"
+                #        exit 1
+                #fi
+
                 ### Check text clustering
 #               cd $WORKSPACE
 #               git clone git@wpp.isd.dp.ua:irls/rrm-processor.git
