80dd9b86020f54095a6e1e2b9918949367ef2e51
Denys Vachov <dvac@isd.dp.ua>
2014-12-02 16:23:06 +0200

    fix determine branch for other environments

--- jenkins-irls/irls-reader-deploy.sh
+++ jenkins-irls/irls-reader-deploy.sh
@@ -2,6 +2,7 @@
 ### This job sets default values facets for further deployment
 ###
 ARTIFACTS_DIR=/home/jenkins/irls-reader-artifacts
+ARTIFACTS_DIR_STAGE=/home/jenkins/irls-reader-artifacts-stage
 TARGETS_REPO="git@wpp.isd.dp.ua:irls/targets.git"
 TARGETS_REPO_DIR_NAME=$(echo $TARGETS_REPO | cut -d":" -f2 | cut -d"/" -f2 | sed s@.git@@g)
 
@@ -17,13 +18,17 @@ deploymentPackageId=($(echo $ID))
 
 for i in "${deploymentPackageId[@]}"
 do
-        BRANCHNAME=$(grep "reader.*:" $ARTIFACTS_DIR/$i/meta.json -A3 | grep "branchName" | awk -F "\"" '{print $4}')
+	if [ "$dest" == "DEVELOPMENT" ] || [ "$dest" == "STAGE" ]; then
+        	BRANCHNAME=$(grep "reader.*:" $ARTIFACTS_DIR/$i/meta.json -A3 | grep "branchName" | awk -F "\"" '{print $4}')
+	elif [ "$dest" == "LIVE" ]; then
+        	BRANCHNAME=$(grep "reader.*:" $ARTIFACTS_DIR_STAGE/$i/meta.json -A3 | grep "branchName" | awk -F "\"" '{print $4}')
+	fi
         rm -f $WORKSPACE/myenv
         echo "BRANCHNAME=$BRANCHNAME" >> $WORKSPACE/myenv
 done
 if [ -z $BRANCHNAME ]; then
-	printf "[ERROR_BRANCH] unable to determine the branch name \n"
-	exit 1
+        printf "[ERROR_BRANCH] unable to determine the branch name \n"
+        exit 1
 fi
 echo TARGET=${TARGET[@]}
 echo "TARGET=${LIST_OF_ALL_TARGETS[@]}" >> $WORKSPACE/myenv
