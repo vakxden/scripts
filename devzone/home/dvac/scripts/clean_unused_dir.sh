bae602664ec8f3ed6d4621d9026142cba48db106
Denys Vachov <dvac@isd.dp.ua>
2014-12-02 16:25:19 +0200

    add branches

--- devzone/home/dvac/scripts/clean_unused_dir.sh
+++ devzone/home/dvac/scripts/clean_unused_dir.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-TMP_FILE="/home/dvac/tmpfile"
-touch $TMP_FILE
-#LIST=$(ps aux | grep node | grep -v grep | awk '{print $12}' | awk -F"[__]" '{print $2}')
-LIST=$(ps aux | grep node | grep -v grep | awk '{print $12}' | sed 's@server\/index_@@g' | sed 's@_develop.js@@g')
-
-for i in $LIST
+        TMP_FILE="/home/dvac/tmpfile"
+        cat /dev/null > $TMP_FILE
+        if [ ! -f $TMP_FILE ]; then touch $TMP_FILE; fi
+for BRANCH in develop master audio
 do
-        VALUE=$(cat /home/dvac/apache2/conf/extra/proxypass-$i-develop.conf | grep 8890 | awk '{print $3}' | awk -F '/' '{print $5}' | sort | uniq)
-        COUNT="$(cat $TMP_FILE | wc -l)"
-        if [ "$COUNT" = "0" ]; then
-                echo "find /home/dvac/irls-reader-artifacts -maxdepth 1 -type d -not -name $VALUE" >> $TMP_FILE
-        else
-                printf " -not -name $VALUE" >> $TMP_FILE
-        fi
-done
 
-COUNT_LINES=$(cat $TMP_FILE | tr '\n' ' ' | xargs -0 bash -c | wc -l)
-TAIL=$(($COUNT_LINES-1))
-cat $TMP_FILE | tr '\n' ' ' | xargs -0 bash -c | tail -"$TAIL" | xargs rm -rf
-rm -f $TMP_FILE
+        LIST=$(ps aux | grep node | grep -v grep | grep "$BRANCH.js" | awk '{print $12}' | sed 's@server\/index_@@g' | sed 's@_'$BRANCH'.js@@g')
+
+        for i in $LIST
+        do
+                VALUE=$(cat /home/dvac/apache2/conf/extra/proxypass-$i-$BRANCH.conf | grep 8890 | awk '{print $3}' | awk -F '/' '{print $5}' | sort | uniq)
+                COUNT="$(cat $TMP_FILE | wc -l)"
+                if [ "$COUNT" = "0" ]; then
+                        echo "find /home/dvac/irls-reader-artifacts -maxdepth 1 -type d -not -name $VALUE" >> $TMP_FILE
+                else
+                        printf " -not -name $VALUE" >> $TMP_FILE
+                fi
+        done
+
+done
+        COUNT_LINES=$(cat $TMP_FILE | tr '\n' ' ' | xargs -0 bash -c | wc -l)
+        TAIL=$(($COUNT_LINES-1))
+        cat $TMP_FILE | tr '\n' ' ' | xargs -0 bash -c | tail -"$TAIL" | xargs rm -rf
+        rm -f $TMP_FILE
