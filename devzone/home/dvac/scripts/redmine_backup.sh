ad3e5b76d67e6d0c7532d3f1d628fa648addc855
Denys Vachov <dvac@isd.dp.ua>
2014-12-22 10:01:05 +0200

    fix redmine backup script

--- devzone/home/dvac/scripts/redmine_backup.sh
+++ devzone/home/dvac/scripts/redmine_backup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-tar cfzh /home/dvac/redmine_backup.$(date +%d-%b-%Y_%H-%m-%S).tar.gz /home/dvac/redmine --exclude='*.log'
+tar cfzh /home/dvac/redmine_backup.$(date +%d-%b-%Y_%H-%m-%S).tar.gz /home/dvac/redmine --exclude='*.log'  > /dev/null 2>&1 &
 function remove_old_archives (){
         # $1 - directory for search of backup files (e.g "/home/dvac")
         # $2 - mask of searching backup files (e.g "redmine_backup.*.tar.gz")
         # Determine of numbers of files $2 in the $1 directory
         NUM=$(ls -f $1/$2 | wc -l)
-        echo NUM=$NUM
+        #echo NUM=$NUM
         # If number of files is more than 2, then we will remove all files except the 2 most recent files
         if (( $NUM > 2 )); then
                 HEAD_NUM=$(($NUM-2))
@@ -16,4 +16,4 @@ function remove_old_archives (){
                 done
         fi
 }
-remove_old_archives /home/dvac redmine_backup.*.tar.gz
+remove_old_archives /home/dvac "redmine_backup.*.tar.gz"
