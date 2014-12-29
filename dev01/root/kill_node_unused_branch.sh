39197dcae044f5b8404b45b772057c0bb8378cc1
Denys Vachov <dvac@isd.dp.ua>
2014-12-29 13:23:27 +0200

    fix path to branches.json

--- dev01/root/kill_node_unused_branch.sh
+++ dev01/root/kill_node_unused_branch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ART_CURRENT_DIR="/home/jenkins/irls-reader-artifacts/"
+BRANCHES_JSON="/home/jenkins/irls-reader-artifacts/branches.json"
 
 # list of names of running node-processes from not-develop branches (branches like as 'feature*' or 'hotfix*' or 'test*')
 LIST=($(ps aux | grep node | egrep -v "STAGE|NIGHT|grep node" | awk '{print $12}' | sed 's@server\/index_@@g' | sed 's@.js@@g'  | sed 's/[a-z_-]*_feature-/feature\//g' | grep -v develop | egrep "feature|hotfix|test" | sort | uniq))
@@ -8,7 +9,7 @@ LIST=($(ps aux | grep node | egrep -v "STAGE|NIGHT|grep node" | awk '{print $12}
 # kill node with unused branches and remove apache proxy- files and artifacts directories
 for i in ${LIST[@]}
 do
-        if ! egrep -q '"'$i'",$|"'$i'"$' branches.json; then
+        if ! egrep -q '"'$i'",$|"'$i'"$' $BRANCHES_JSON; then
                 ps aux | grep node.*$(echo $i | sed -e 's@^feature\/@feature-@g' -e 's@^hotfix\/@hotfix-@g' -e 's@^test\/@test-@g') | grep -v grep | awk '{print $2}' | xargs kill -9
                 echo "branch $i is unused, kill the node process"
                 y=$(echo $i | sed -e 's@^feature\/@feature-@g' -e 's@^hotfix\/@hotfix-@g' -e 's@^test\/@test-@g'); APACHEFILE=($( ls -f /etc/apache2/sites-enabled/irls-current-reader-*-$y))
