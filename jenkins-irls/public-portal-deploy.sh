43a509a9d89b4606edc6bda35343ce3d89566770
Denys Vachov <dvac@isd.dp.ua>
2015-01-13 19:37:31 +0200

    rsync for public-portal-deploy.sh (to avoid overwrite of file build.version.json)

--- jenkins-irls/public-portal-deploy.sh
+++ jenkins-irls/public-portal-deploy.sh
@@ -1,3 +1,4 @@
-ssh dvac@devzone.dp.ua "rm -rf /home/dvac/apache2/var/www/portal/*"
-scp -r $WORKSPACE/* dvac@devzone.dp.ua:/home/dvac/apache2/var/www/portal/
+#ssh dvac@devzone.dp.ua "rm -rf /home/dvac/apache2/var/www/portal/*"
+#scp -r $WORKSPACE/* dvac@devzone.dp.ua:/home/dvac/apache2/var/www/portal/
+time rsync -rzv --delete --exclude "build.version.json" --exclude ".git" -e "ssh" $WORKSPACE/ dvac@devzone.dp.ua:/home/dvac/apache2/var/www/portal/
 ssh dvac@devzone.dp.ua "/home/dvac/apache2/bin/apachectl restart"
