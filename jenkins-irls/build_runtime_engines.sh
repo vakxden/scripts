ac2869223d1a88079c748f6055917d467d0e2d09
Denys Vachov <dvac@isd.dp.ua>
2015-01-22 18:52:39 +0200

    add build_runtime_engines.sh

---
+++ jenkins-irls/build_runtime_engines.sh
@@ -0,0 +1,36 @@
+if [ -z $REPONAME ]; then
+        echo \[ERROR_REPO\] reponame not passed!
+        exit 1
+fi
+
+if [ -z $BRANCH ]; then
+        echo \[ERROR_BRANCH\] branch not passed!
+        exit 1
+fi
+
+function git_clone {
+        cd $WORKSPACE
+        git clone git@wpp.isd.dp.ua:irls/$REPONAME.git
+        }
+
+function git_checkout {
+        cd $WORKSPACE/$REPONAME
+        git reset --hard
+        git clean -fdx
+        git fetch --all
+        git checkout $BRANCH
+        }
+
+
+if [ ! -d $WORKSPACE/$REPONAME ]; then
+	git_clone
+	git_checkout
+else
+	git_checkout
+fi
+
+ssh jenkins@yuriys-mac-mini.isd.dp.ua "if [ ! -d ~/git/$REPONAME ]; then mkdir -p ~/git/$REPONAME ; fi"
+time rsync -rz --delete -e "ssh" $WORKSPACE/$REPONAME/ jenkins@yuriys-mac-mini.isd.dp.ua:~/git/$REPONAME
+ssh jenkins@dev02.design.isd.dp.ua "if [ ! -d ~/git/$REPONAME ]; then mkdir -p ~/git/$REPONAME ; fi"
+time rsync -rz --delete -e "ssh" $WORKSPACE/$REPONAME/ jenkins@dev02.design.isd.dp.ua:~/git/$REPONAME
+
