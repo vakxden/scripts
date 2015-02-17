4106e00e2b04ea36134885236da3f52e4463f0e6
Denys Vachov <dvac@isd.dp.ua>
2015-02-17 12:03:20 +0200

    update build_runtime_engines.sh

--- jenkins-irls/build_runtime_engines.sh
+++ jenkins-irls/build_runtime_engines.sh
@@ -18,19 +18,23 @@ function git_checkout {
         git reset --hard
         git clean -fdx
         git fetch --all
-        git checkout $BRANCH
+        git checkout remotes/origin/$BRANCH
         }
 
 
 if [ ! -d $WORKSPACE/$REPONAME ]; then
-	git_clone
-	git_checkout
+        git_clone
+        git_checkout
 else
-	git_checkout
+        git_checkout
 fi
 
 ssh jenkins@yuriys-mac-mini.isd.dp.ua "if [ ! -d ~/git/$REPONAME ]; then mkdir -p ~/git/$REPONAME ; fi"
 time rsync -rz --delete -e "ssh" $WORKSPACE/$REPONAME/ jenkins@yuriys-mac-mini.isd.dp.ua:~/git/$REPONAME
 ssh jenkins@dev02.design.isd.dp.ua "if [ ! -d ~/git/$REPONAME ]; then mkdir -p ~/git/$REPONAME ; fi"
 time rsync -rz --delete -e "ssh" $WORKSPACE/$REPONAME/ jenkins@dev02.design.isd.dp.ua:~/git/$REPONAME
-
+ssh jenkins@irls-autotests.design.isd.dp.ua "if [ ! -d ~/git/$REPONAME ]; then mkdir -p ~/git/$REPONAME ; fi"
+time rsync -rz --delete -e "ssh" $WORKSPACE/$REPONAME/ jenkins@irls-autotests.design.isd.dp.ua:~/git/$REPONAME
+# scp to users-mac-mini
+ssh jenkins@users-Mac-mini.design.isd.dp.ua "if [ ! -d ~/git/$REPONAME ]; then mkdir -p ~/git/$REPONAME ; fi"
+time rsync -rz --delete -e "ssh" $WORKSPACE/$REPONAME/ jenkins@users-Mac-mini.design.isd.dp.ua:~/git/$REPONAME
