dfd7d082f0a307421803d816f08ba8d4806bb0b6
Denys Vachov <dvac@isd.dp.ua>
2015-02-24 19:25:14 +0200

    running grunt task with two parameters

--- testgrunt/Gruntfile.js
+++ testgrunt/Gruntfile.js
@@ -1,4 +1,4 @@
-// this running with next command: " grunt --reponame=test_for_grunt gitTask:./test_for_grunt"
+//run with next command: "time grunt --reponame=test_for_grunt --branchname=develop"
 
 module.exports = function(grunt) {
 
@@ -6,42 +6,67 @@ grunt.initConfig({
     gitclone: {
         clone: {
             options: {
-                repository: 'git@wpp.isd.dp.ua:irls/<%= grunt.option("reponame") %>',
-                branch: 'develop',
-                directory: '<%= grunt.option("directory") %>'
+                repository: 'git@wpp.isd.dp.ua:irls/<%= grunt.option("reponame") %>.git',
+                branch: '<%= grunt.option("branchname") %>',
+                directory: '<%= grunt.option("dirname") %>'
+            }
+        }
+    },
+    gitreset: {
+        with_hard_option: {
+            options: {
+                mode: 'hard'
+            }
+        }
+    },
+    gitclean: {
+        with_fdx_options: {
+            options: {
+                force: true,
+                directories: true,
+                nonstandard: true
+            }
+        }
+    },
+    gitfetch: {
+        with_all_option: {
+            options: {
+                all: true
             }
         }
     },
     gitcheckout: {
         task: {
             options: {
-                branch: 'develop',
-                cwd: '<%= grunt.option("directory") %>'
+                branch: 'remotes/origin/<%= grunt.option("branchname") %>'
             }
         }
     },
 });
 
 
-grunt.registerTask('gitTask', 'Git', function(directory) {
+var branchname = grunt.option('branchname');
+console.log("received branchname is " + branchname);
+var reponame = grunt.option('reponame');
+console.log("received reponame is " + reponame);
+var dirname = './'+reponame;
+console.log("dirname is " + dirname);
+
+grunt.registerTask('gitTask', 'Git', function(dirname) {
+    console.log("function dirname is " + dirname);
     var fs=require('fs');
-    var exists = fs.existsSync(directory);
-    console.log(directory);
-    grunt.option('directory', directory);
-    var reponame = grunt.option('reponame');
-    console.log(reponame);
+    var exists = fs.existsSync(dirname);
     grunt.loadNpmTasks('grunt-git');
     if(exists){
         console.log('yes');
-        grunt.task.run('gitcheckout');
+        process.chdir(dirname);
+        grunt.task.run('gitreset', 'gitclean', 'gitfetch','gitcheckout');
     }else{
         console.log("no");
-        grunt.task.run('gitclone', 'gitcheckout');
+        grunt.task.run('gitclone');
     }
 });
 
-//var directory = './build_re';
-grunt.registerTask('default', ['gitTask']);
+grunt.registerTask('default', ['gitTask:' + dirname]);
 
 };
-
