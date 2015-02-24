954e6106828f7ae1a28b1c082f630102c1351ec0
Denys Vachov <dvac@isd.dp.ua>
2015-02-24 18:18:20 +0200

    running grunt task with parameter

--- testgrunt/Gruntfile.js
+++ testgrunt/Gruntfile.js
@@ -1,3 +1,4 @@
+// this running with next command: "grunt gitTask:./build_re
 
 module.exports = function(grunt) {
 
@@ -7,34 +8,37 @@ grunt.initConfig({
             options: {
                 repository: 'git@wpp.isd.dp.ua:irls/build_re.git',
                 branch: 'develop',
-                directory: 'build_re'
+                directory: '<%= grunt.option("directory") %>'
             }
         }
     },
     gitcheckout: {
         task: {
             options: {
-                branch: 'remotes/origin/develop',
-                create: true,
-		cwd: "./build_re"
+                branch: 'develop',
+                cwd: '<%= grunt.option("directory") %>'
             }
         }
     },
 });
 
-grunt.registerTask('gitTask', 'Git', function() {   
+
+grunt.registerTask('gitTask', 'Git', function(directory) {
     var fs=require('fs');
-    var exists = fs.existsSync('./build_re');
-        grunt.loadNpmTasks('grunt-git');
-        if(exists){
-            console.log('yes');
-            grunt.task.run('gitcheckout');
-        }else{
-            console.log("no");
-            grunt.task.run('gitclone', 'gitcheckout');
-        }
+    var exists = fs.existsSync(directory);
+    console.log(directory);
+    grunt.option('directory', directory);
+    grunt.loadNpmTasks('grunt-git');
+    if(exists){
+        console.log('yes');
+        grunt.task.run('gitcheckout');
+    }else{
+        console.log("no");
+        grunt.task.run('gitclone', 'gitcheckout');
+    }
 });
 
+//var directory = './build_re';
 grunt.registerTask('default', ['gitTask']);
 
 };
