1990236a0114061f6468a87c163a8871fd0d4c51
Denys Vachov <dvac@isd.dp.ua>
2015-03-26 16:54:49 +0200

    add some tasks to processor gruntfile

--- newbuild/lib-processor-Gruntfile.js
+++ newbuild/lib-processor-Gruntfile.js
@@ -1,5 +1,5 @@
 //example
-//grunt --git_commit=213df8390d77d4ef8fe8071a8bebe784057859e4 --commit_message="Feature #817 Vocabulary assessment drops facet" --branchname=develop --commit_author="Gennadiy Sherstyuk" --commit_date="2015-03-24 09:42:01 +0200" --email="gshe@isd.dp.ua" --commit_url="http://wpp.isd.dp.ua/gitlab/irls/lib-processor/commit/213df8390d77d4ef8fe8071a8bebe784057859e4" --meta_json_file="lib-processor-meta.json" --current_code_dir=/home/jenkins/irls-lib-processor-deploy/213df8390d77d4ef8fe8071a8bebe784057859e4
+//  grunt --git_commit=213df8390d77d4ef8fe8071a8bebe784057859e4 --commit_message="Feature #817 Vocabulary assessment drops facet" --branchname=develop --commit_author="Gennadiy Sherstyuk" --commit_date="2015-03-24 09:42:01 +0200" --email="gshe@isd.dp.ua" --commit_url="http://wpp.isd.dp.ua/gitlab/irls/lib-processor/commit/213df8390d77d4ef8fe8071a8bebe784057859e4" --meta_json_file="lib-processor-meta.json" --current_code_dir=/home/jenkins/irls-lib-processor-deploy/213df8390d77d4ef8fe8071a8bebe784057859e4 --current_code_path=/home/jenkins/irls-lib-processor-deploy
 
 /*global module: false*/
 module.exports = function(grunt)
@@ -41,7 +41,16 @@ module.exports = function(grunt)
         grunt.loadNpmTasks('grunt-jscs-checker');
         grunt.loadNpmTasks("grunt-rsync");
 
-        var current_code_dir = grunt.option ('current_code_dir');
+        var current_code_path = grunt.option ('current_code_path');
+        var git_commit = grunt.option ('git_commit');
+        var commit_message = grunt.option ('commit_message');
+        var branchname = grunt.option ('branchname');
+        var commit_author = grunt.option ('commit_author');
+        var commit_date = grunt.option ('commit_date');
+        var email = grunt.option ('email');
+        var commit_url = grunt.option ('commit_url');
+        var meta_json_file = grunt.option ('meta_json_file');
+        var current_code_dir = current_code_path+'/'+git_commit;
 
         grunt.registerTask('rsync_to_current_code_dir', 'sync current snapshot (commit) to current code directory', function(current_code_dir) {
                 var fs=require('fs');
@@ -55,16 +64,48 @@ module.exports = function(grunt)
                         grunt.task.run('rsync:irls_lib_processor_deploy');
                 }
         });
+        grunt.registerTask('current_code_dir_clean', 'clean of old directories', function(current_code_path) {
+                var fs=require('fs');
+                var path = require("path");
+                var rmdir = function(dir) {
+                        var list = fs.readdirSync(dir);
+                        for(var i = 0; i < list.length; i++) {
+                                var filename = path.join(dir, list[i]);
+                                var stat = fs.statSync(filename);
+                                if(filename == "." || filename == "..") {
+                                } else if(stat.isDirectory()) {
+                                        rmdir(filename);
+                                } else {
+                                        fs.unlinkSync(filename);
+                                }
+                        }
+                fs.rmdirSync(dir);
+                };
+                grunt.option ('current_code_path', current_code_path);
+                if(current_code_path){
+                        process.chdir(current_code_path);
+                }
+                var dirContents = fs.readdirSync('.');
+                var dirs = [];
+                var i;
+                if(dirContents){
+                        for(i=0; i<dirContents.length; i++){
+                                var stats = fs.statSync(dirContents[i]);
+                                if(stats.isDirectory()){
+                                        dirs.push(stats.mtime.getTime()+'/'+dirContents[i]);
+                                }
+                        }
+                        dirs.sort();
+                        for(i=0; i<5; i++){
+                                dirs.pop();
+                        }
+                        for(i=0; i<dirs.length; i++){
+                                rmdir(dirs[i].replace(/^\d+\//,''));
+                        }
+                };
+        });
 
         grunt.registerTask('create_meta', 'Creates meta json file', function() {
-                var git_commit = grunt.option ('git_commit');
-                var commit_message = grunt.option ('commit_message');
-                var branchname = grunt.option ('branchname');
-                var commit_author = grunt.option ('commit_author');
-                var commit_date = grunt.option ('commit_date');
-                var email = grunt.option ('email');
-                var commit_url = grunt.option ('commit_url');
-                var meta_json_file = grunt.option ('meta_json_file');
                 var jsonBody = {
                         "Processor": {
                                 commitID: git_commit,
@@ -82,5 +123,5 @@ module.exports = function(grunt)
         });
 
         grunt.registerTask('verify', ['jshint', 'jscs']);
-        grunt.registerTask('default', ['verify', 'create_meta', 'rsync_to_current_code_dir:'+current_code_dir]);
+        grunt.registerTask('default', ['verify', 'create_meta', 'rsync_to_current_code_dir:'+current_code_dir,'current_code_dir_clean:'+current_code_path]);
 };
