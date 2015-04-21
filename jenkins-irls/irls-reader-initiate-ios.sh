d51c03d6e0904f20923e8b952b2ab6f03acb9081
Denys Vachov <dvac@isd.dp.ua>
2015-04-21 18:09:23 +0300

    delete feature/cordova condition

--- jenkins-irls/irls-reader-initiate-ios.sh
+++ jenkins-irls/irls-reader-initiate-ios.sh
@@ -5,11 +5,12 @@ export PATH=$PATH:/usr/local/bin
 export HTTP_PROXY=http://10.98.192.120:3128
 export HTTPS_PROXY=http://10.98.192.120:3128
 CURRENT_BUILD=/Users/jenkins/irls-reader-current-build
-if [ ! -d /Users/jenkins/irls-reader-current-epubs/$BRANCHNAME ]; then
+if [ "$BRANCHNAME" != "master" ]; then
         CURRENT_EPUBS=/Users/jenkins/irls-reader-current-epubs/develop
 else
         CURRENT_EPUBS=/Users/jenkins/irls-reader-current-epubs/$BRANCHNAME
 fi
+
 ARTIFACTS_DIR=/home/jenkins/irls-reader-artifacts # it's directory locates on the host dev01
 echo SHELL=$BASH
 echo SHELL_VERSION=$BASH_VERSION
@@ -47,21 +48,17 @@ done
 function main_loop {
         notmainloop ()
         {
-		BRAND=$(grep brand $WORKSPACE/targets/$i/targetConfig.json | awk -F '"|"' '{print $4}')
-		IPA_NAME=$BRANCH-$BRAND\_Reader-$i
-		if [ ! -d $WORKSPACE/build/build ]; then mkdir -p $WORKSPACE/build/build; fi
-		if [ "$BRANCHNAME" == "feature/cordova" ]; then
-			cp -Rf ~/build_re/$BRANCHNAME/phonegap-plugins $WORKSPACE/build/build
-		else
-			cp -Rf ~/build_re/develop/phonegap-plugins $WORKSPACE/build/build
-		fi
+                BRAND=$(grep brand $WORKSPACE/targets/$i/targetConfig.json | awk -F '"|"' '{print $4}')
+                IPA_NAME=$BRANCH-$BRAND\_Reader-$i
+                if [ ! -d $WORKSPACE/build/build ]; then mkdir -p $WORKSPACE/build/build; fi
+                cp -Rf ~/build_re/$BRANCHNAME/phonegap-plugins $WORKSPACE/build/build
                 cd $WORKSPACE/build
-		if [ $BRANCHNAME == "master" ];
+                if [ $BRANCHNAME == "master" ];
                 then
-                	time node index.js --platform=ios --config=$WORKSPACE/targets --from=$WORKSPACE/client --prefix=$BRANCH- --epubs=$CURRENT_EPUBS
-		else
-                	time node index.js --platform=ios --config=$WORKSPACE/targets --from=$WORKSPACE/client --prefix=$BRANCH- --epubs=$CURRENT_EPUBS --buildnumber=$BUILD_NUMBER --builddate="$BUILD_DATE"
-		fi
+                        time node index.js --platform=ios --config=$WORKSPACE/targets --from=$WORKSPACE/client --prefix=$BRANCH- --epubs=$CURRENT_EPUBS
+                else
+                        time node index.js --platform=ios --config=$WORKSPACE/targets --from=$WORKSPACE/client --prefix=$BRANCH- --epubs=$CURRENT_EPUBS --buildnumber=$BUILD_NUMBER --builddate="$BUILD_DATE"
+                fi
                 #unlock keychain
                 security unlock-keychain -p jenk123ins /Users/jenkins/Library/Keychains/login.keychain
                 #build with xcodebuild
@@ -69,31 +66,31 @@ function main_loop {
                 #create ipa-file
                 time /usr/bin/xcrun -sdk iphoneos8.1 PackageApplication -v "$WORKSPACE/build/$IPA_NAME.app" -o $WORKSPACE/$IPA_NAME.ipa --embed $MOBILEPROVISION --sign "$CODE_SIGN_IDENTITY"
                 rm -f $WORKSPACE/$IPA_NAME*debug.ipa
-		if [ -f $WORKSPACE/$IPA_NAME.ipa ]; then
-			ssh jenkins@dev01.isd.dp.ua "if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts ]; then mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts; fi"
-                	time scp $WORKSPACE/$IPA_NAME.ipa jenkins@dev01.isd.dp.ua:$ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts/
-			rm -f $WORKSPACE/$IPA_NAME.ipa
-			printf "File $WORKSPACE/$IPA_NAME.ipa moved to host dev01.isd.dp.ua \n"
-		else
-			printf "[ERROR_FILE_EXIST] File $WORKSPACE/$IPA_NAME.ipa not found!!! \n"
-		fi
-		rm -f $WORKSPACE/$IPA_NAME.ipa
+                if [ -f $WORKSPACE/$IPA_NAME.ipa ]; then
+                        ssh jenkins@dev01.isd.dp.ua "if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts ]; then mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts; fi"
+                        time scp $WORKSPACE/$IPA_NAME.ipa jenkins@dev01.isd.dp.ua:$ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts/
+                        rm -f $WORKSPACE/$IPA_NAME.ipa
+                        printf "File $WORKSPACE/$IPA_NAME.ipa moved to host dev01.isd.dp.ua \n"
+                else
+                        printf "[ERROR_FILE_EXIST] File $WORKSPACE/$IPA_NAME.ipa not found!!! \n"
+                fi
+                rm -f $WORKSPACE/$IPA_NAME.ipa
                 rm -rf $CONFIGURATION_BUILD_DIR/*
         }
 
-	for i in "${!combineArray[@]}"
-	do
+        for i in "${!combineArray[@]}"
+        do
                 rm -rf $WORKSPACE/*
                 GIT_COMMIT_TARGET=$(echo "$GIT_COMMIT"-"$i")
-		if [ ! -d $CURRENT_BUILD/$GIT_COMMIT_TARGET ]; then
-			echo "[ERROR_INITIATE] Directory not found! Maybe for this target ($i) disabled option platform:ios."
-			exit 1
-		else
-                	cp -Rf $CURRENT_BUILD/$GIT_COMMIT_TARGET/* $WORKSPACE/
-			echo $i --- ${combineArray[$i]}
-			notmainloop
-		fi
-	done
+                if [ ! -d $CURRENT_BUILD/$GIT_COMMIT_TARGET ]; then
+                        echo "[ERROR_INITIATE] Directory not found! Maybe for this target ($i) disabled option platform:ios."
+                        exit 1
+                else
+                        cp -Rf $CURRENT_BUILD/$GIT_COMMIT_TARGET/* $WORKSPACE/
+                        echo $i --- ${combineArray[$i]}
+                        notmainloop
+                fi
+        done
 }
 
 main_loop
