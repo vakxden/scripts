c1c6ea9bf4e08b3e3fbe7e40c424b00457202498
Denys Vachov <dvac@isd.dp.ua>
2015-04-22 10:25:20 +0300

    housekeeping

--- jenkins-irls/irls-reader-initiate-android.sh
+++ jenkins-irls/irls-reader-initiate-android.sh
@@ -7,11 +7,6 @@ export HTTPS_PROXY="http://10.98.192.120:3128"
 export https_proxy="http://10.98.192.120:3128"
 export HTTP_PROXY="http://10.98.192.120:3128"
 export http_proxy="http://10.98.192.120:3128"
-###
-### export NODE_HOME=/opt/node
-### export ANDROID_HOME=/opt/android-sdk-linux
-### export PATH=$PATH:$NODE_HOME/bin:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$JAVA_HOME/bin
-###
 export NODE_HOME=/opt/node
 export CORDOVA_ARM=/home/jenkins/crosswalk_environment/crosswalk-cordova-11.40.277.7-arm
 export PATH=/home/jenkins/crosswalk_environment/ant/bin:/home/jenkins/crosswalk_environment/jdk1.7.0_76/bin:$NODE_HOME/bin:$PATH
@@ -23,6 +18,7 @@ export PATH=/home/jenkins/crosswalk_environment/android-sdk-linux/platform-tools
 BRANCH=$(echo $BRANCHNAME | sed 's/\//-/g' | sed 's/_/-/g')
 ARTIFACTS_DIR=/home/jenkins/irls-reader-artifacts
 TARGETS_REPONAME="targets"
+### Determine path to epubs books
 if [ ! -d $HOME/irls-reader-current-epubs/$BRANCHNAME ]; then
         CURRENT_EPUBS=$HOME/irls-reader-current-epubs/develop
 else
@@ -57,6 +53,7 @@ function main_loop {
         notmainloop ()
         {
                 if [ ! -d $WORKSPACE/build/build ]; then mkdir -p $WORKSPACE/build/build; fi
+		### Determine of build runtime engines path
 		if [ -d ~/build_re/$BRANCHNAME ]; then
                 	cp -Rf ~/build_re/$BRANCHNAME/phonegap-plugins $WORKSPACE/build/build/
                 	cp -Rf ~/build_re/$BRANCHNAME/android $WORKSPACE/build/build/
@@ -69,23 +66,23 @@ function main_loop {
                 then
                         time node index.js --platform=android --config=$WORKSPACE/targets --from=$WORKSPACE/client --prefix=$BRANCH- --epubs=$CURRENT_EPUBS
                 else
-						$CORDOVA_ARM/bin/create $i org.crosswalkproject.$(echo $i | sed 's/-/_/g') $BRANCH
+			$CORDOVA_ARM/bin/create $i org.crosswalkproject.$(echo $i | sed 's/-/_/g') $BRANCH
                         time node index.js --platform=android --config=$WORKSPACE/targets --from=$WORKSPACE/client --manifest=$WORKSPACE/client/package.json --prefix=$BRANCH- --epubs=$CURRENT_EPUBS --crosswalk=/home/jenkins/crosswalk_environment/crosswalk-11.40.277.7
-						$WORKSPACE/build/$i/cordova/build
+			$WORKSPACE/build/$i/cordova/build
                 fi
-				### Determine of brand
-				BRAND=$(grep brand $WORKSPACE/$TARGETS_REPONAME/$i/targetConfig.json | awk -F '"|"' '{print $4}')
-				### Determine of apk name
-				APK_NAME=$(echo $BRANCH-"$BRAND"_Reader-$i)
-				APK_FILE_NAME="$APK_NAME.apk"
-				### Copying builded apk to workspace
+		### Determine of brand
+		BRAND=$(grep brand $WORKSPACE/$TARGETS_REPONAME/$i/targetConfig.json | awk -F '"|"' '{print $4}')
+		### Determine of apk name
+		APK_NAME=$(echo $BRANCH-"$BRAND"_Reader-$i)
+		APK_FILE_NAME="$APK_NAME.apk"
+		### Copying builded apk to workspace
                 cp $WORKSPACE/build/$i/out/*.apk $WORKSPACE/$APK_FILE_NAME
                 ssh jenkins@dev01.isd.dp.ua "
                 if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts ]; then
                         mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts
                 fi
                 "
-				### Copying builded apk to artifacts directory
+		### Copying builded apk to artifacts directory
                 time scp $WORKSPACE/$APK_FILE_NAME  jenkins@dev01.isd.dp.ua:$ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts/ && rm -f $WORKSPACE/$BRANCH-$i.apk
         }
 
