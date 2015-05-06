0d11a5743de44f661306ed5c4da881c856b6f9c0
Denys Vachov <dvac@isd.dp.ua>
2015-05-06 10:05:03 +0300

    update crosswalk paths

--- jenkins-irls/irls-reader-initiate-android.sh
+++ jenkins-irls/irls-reader-initiate-android.sh
@@ -8,9 +8,12 @@ export https_proxy="http://10.98.192.120:3128"
 export HTTP_PROXY="http://10.98.192.120:3128"
 export http_proxy="http://10.98.192.120:3128"
 export NODE_HOME=/opt/node
-export CROSSWALK=/home/jenkins/crosswalk_environment/crosswalk-12.41.296.9
-export CORDOVA_ARM=/home/jenkins/crosswalk_environment/crosswalk-cordova-12.41.296.9-arm
-export CORDOVA_X86=/home/jenkins/crosswalk_environment/crosswalk-cordova-12.41.296.9-x86
+#export CROSSWALK=/home/jenkins/crosswalk_environment/crosswalk-12.41.296.9
+export CROSSWALK=/home/jenkins/crosswalk_environment/crosswalk-13.42.319.10
+#export CORDOVA_ARM=/home/jenkins/crosswalk_environment/crosswalk-cordova-12.41.296.9-arm
+export CORDOVA_ARM=/home/jenkins/crosswalk_environment/crosswalk-cordova-13.42.319.10-arm
+#export CORDOVA_X86=/home/jenkins/crosswalk_environment/crosswalk-cordova-12.41.296.9-x86
+export CORDOVA_X86=/home/jenkins/crosswalk_environment/crosswalk-cordova-13.42.319.10-x86
 export PATH=/home/jenkins/crosswalk_environment/ant/bin:/home/jenkins/crosswalk_environment/jdk1.7.0_76/bin:$NODE_HOME/bin:$PATH
 export JAVA_HOME=/home/jenkins/crosswalk_environment/jdk1.7.0_76
 export PATH=/home/jenkins/crosswalk_environment/android-sdk-linux:$PATH
@@ -54,37 +57,37 @@ done
 function main_loop {
         notmainloop ()
         {
-		### Determine of brand
-		BRAND=$(grep brand $WORKSPACE/$TARGETS_REPONAME/$i/targetConfig.json | awk -F '"|"' '{print $4}')
-		### Determine of apk name
-		APK_NAME=$(echo $BRANCH-"$BRAND"_Reader-$i)
-		APK_FILE_NAME="$APK_NAME.apk"
+                ### Determine of brand
+                BRAND=$(grep brand $WORKSPACE/$TARGETS_REPONAME/$i/targetConfig.json | awk -F '"|"' '{print $4}')
+                ### Determine of apk name
+                APK_NAME=$(echo $BRANCH-"$BRAND"_Reader-$i)
+                APK_FILE_NAME="$APK_NAME.apk"
                 if [ ! -d $WORKSPACE/build/build ]; then mkdir -p $WORKSPACE/build/build; fi
-		### Determine of build runtime engines path
-		if [ -d ~/build_re/$BRANCHNAME ]; then
-                	cp -Rf ~/build_re/$BRANCHNAME/phonegap-plugins $WORKSPACE/build/build/
-                	cp -Rf ~/build_re/$BRANCHNAME/android $WORKSPACE/build/build/
+                ### Determine of build runtime engines path
+                if [ -d ~/build_re/$BRANCHNAME ]; then
+                        cp -Rf ~/build_re/$BRANCHNAME/phonegap-plugins $WORKSPACE/build/build/
+                        cp -Rf ~/build_re/$BRANCHNAME/android $WORKSPACE/build/build/
                 else
-                	cp -Rf ~/build_re/develop/phonegap-plugins $WORKSPACE/build/build/
-                	cp -Rf ~/build_re/develop/android $WORKSPACE/build/build/
+                        cp -Rf ~/build_re/develop/phonegap-plugins $WORKSPACE/build/build/
+                        cp -Rf ~/build_re/develop/android $WORKSPACE/build/build/
                 fi
                 cd $WORKSPACE/build
                 if [ $BRANCHNAME == "master" ];
                 then
                         time node index.js --platform=android --config=$WORKSPACE/targets --from=$WORKSPACE/client --prefix=$BRANCH- --epubs=$CURRENT_EPUBS
                 else
-			$CORDOVA_ARM/bin/create $i org.crosswalkproject.$(echo $i | sed 's/-/_/g') $APK_NAME
+                        $CORDOVA_ARM/bin/create $i org.crosswalkproject.$(echo $i | sed 's/-/_/g') $APK_NAME
                         time node index.js --platform=android --config=$WORKSPACE/targets --from=$WORKSPACE/client --manifest=$WORKSPACE/client/package.json --prefix=$BRANCH- --epubs=$CURRENT_EPUBS --crosswalk=$CROSSWALK
-			$WORKSPACE/build/$i/cordova/build
+                        $WORKSPACE/build/$i/cordova/build
                 fi
-		### Copying builded apk to workspace
+                ### Copying builded apk to workspace
                 cp $WORKSPACE/build/$i/out/*.apk $WORKSPACE/$APK_FILE_NAME
                 ssh jenkins@dev01.isd.dp.ua "
                 if [ ! -d $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts ]; then
                         mkdir -p $ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts
                 fi
                 "
-		### Copying builded apk to artifacts directory
+                ### Copying builded apk to artifacts directory
                 time scp $WORKSPACE/$APK_FILE_NAME  jenkins@dev01.isd.dp.ua:$ARTIFACTS_DIR/${combineArray[$i]}/packages/artifacts/ && rm -f $WORKSPACE/$BRANCH-$i.apk
         }
 
