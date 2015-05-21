8d7b5e7b3809ce6aea3b3b775c38a8cbbd488f17
Denys Vachov <dvac@isd.dp.ua>
2015-05-21 10:08:34 +0300

    changed gitlab backup

--- dev01/root/backup.sh
+++ dev01/root/backup.sh
@@ -9,24 +9,34 @@
 #
 
 # if file older more 3 day, then rename it
+export GEM_HOME=/home/git/gitlab/vendor/bundle/ruby/2.1.0
+export GEM_PATH=/home/git/gitlab/vendor/bundle/ruby/2.1.0
+export NODE_PATH=/opt/node
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:$GEM_HOME/bin:$NODE_PATH/bin
 BACKUP_GITLAB_PATH="/fs/backup/gitlab";
 cd $BACKUP_GITLAB_PATH
 for i in $(find ./ -name "gitlab_backup_[0-9][0-9]*.tar" | sed "s/\.\///g"); do
-        if test $(find $BACKUP_PATH -name $i -mtime +3); then mv "$i" "$i-old"; fi
+        if test $(find $BACKUP_PATH -name $i -mtime +1); then mv "$i" "$i-old"; fi
 done
-# creating gitlab tar-archive
-cd /home/git/gitlab
-sudo -u git bundle exec rake gitlab:backup:create RAILS_ENV=production && mv $BACKUP_GITLAB_PATH/*gitlab_backup.tar  $BACKUP_GITLAB_PATH/gitlab_backup_`date +%d-%b-%y_%H-%M-%S`.tar
 # delete old files
 if [ "$(echo $?)" == "0" ]; then
         rm -f $BACKUP_GITLAB_PATH/gitlab_backup*.tar-old
 fi
+# creating gitlab tar-archive
+cd /home/git/gitlab
+#env | grep -i ruby
+#which node
+#which bundle
+#which gem
+#gem search -l execjs
+#echo "================================"
+sudo -u git bundle exec rake gitlab:backup:create RAILS_ENV=production && mv $BACKUP_GITLAB_PATH/*gitlab_backup.tar  $BACKUP_GITLAB_PATH/gitlab_backup_`date +%d-%b-%y_%H-%M-%S`.tar
 
 ##redmine database backup:
-cd /home/redmine
-mysqldump redmine | gzip > /fs/backup/redmine_mysqldump_`date +%d-%b-%y_%H-%M-%S`.gz
+#cd /home/redmine
+#mysqldump redmine | gzip > /fs/backup/redmine_mysqldump_`date +%d-%b-%y_%H-%M-%S`.gz
 
-##backuping SVN
+#backuping SVN
 rsync -rtv /var/svn/ /fs/backup/svn/
 
 #mysql ( password in /root/.my.cnf )
