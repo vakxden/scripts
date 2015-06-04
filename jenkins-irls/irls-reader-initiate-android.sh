7e3cac494e2f4ee99f87ff7016221fe28fcf7f6a
Denys Vachov <dvac@isd.dp.ua>
2015-06-04 18:33:04 +0300

    add the necessary files for crosswalk

--- jenkins-irls/irls-reader-initiate-android.sh
+++ jenkins-irls/irls-reader-initiate-android.sh
@@ -76,7 +76,9 @@ function main_loop {
                 then
                         time node index.js --platform=android --config=$WORKSPACE/targets --from=$WORKSPACE/client --prefix=$BRANCH- --epubs=$CURRENT_EPUBS
                 else
-                        cp -Rf $WORKSPACE/build/build/android/*.java $CORDOVA_ARM/bin/templates/project
+			# add the necessary files
+                        cp -f $WORKSPACE/build/build/android/*.java $CORDOVA_ARM/bin/templates/project/
+			cp -f $WORKSPACE/build/build/android/create.js $CORDOVA_ARM/bin/lib/create.js
                         $CORDOVA_ARM/bin/create $i org.crosswalkproject.$(echo $i | sed 's/-/_/g') --enable-remote-debugging $APK_NAME
                         time node index.js --platform=android --config=$WORKSPACE/targets --from=$WORKSPACE/client --manifest=$WORKSPACE/client/package.json --prefix=$BRANCH- --epubs=$CURRENT_EPUBS --crosswalk=$CROSSWALK
                         cd $i
