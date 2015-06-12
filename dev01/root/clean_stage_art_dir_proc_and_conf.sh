4cacd3b5ae2a293bb72b00e2b654849be69c1b77
Denys Vachov <dvac@isd.dp.ua>
2015-06-12 11:42:37 +0300

    fix cleaning scripts

--- dev01/root/clean_stage_art_dir_proc_and_conf.sh
+++ dev01/root/clean_stage_art_dir_proc_and_conf.sh
@@ -1,13 +1,15 @@
-urrent artifacts directory
-CURRENT_ART_DIR="/home/jenkins/irls-reader-artifacts"
-BRANCHES_JSON="/home/jenkins/irls-reader-artifacts/branches.json"
-SIZE=15728640 # 15 Gb
+#!/bin/bash
 
-if (($(du -s $CURRENT_ART_DIR/ | awk '{print $1}') > $SIZE));
+# Current artifacts directory
+STAGE_ART_DIR="/home/jenkins/irls-reader-artifacts-stage"
+BRANCHES_JSON="/home/jenkins/irls-reader-artifacts/branches.json"
+SIZE=5242880 # 5 Gb
+
+if (($(du -s $STAGE_ART_DIR/ | awk '{print $1}') > $SIZE));
 then
         echo "Before:"
-        echo "$CURRENT_ART_DIR used $(du -s $CURRENT_ART_DIR/ | awk '{print $1}') kilobytes"
-        echo "$CURRENT_ART_DIR used more then $SIZE kilobytes"
+        echo "$STAGE_ART_DIR used $(du -s $STAGE_ART_DIR/ | awk '{print $1}') kilobytes"
+        echo "$STAGE_ART_DIR used more then $SIZE kilobytes"
         echo ""
         SCRIPT_NAME=$(basename $0)
         TMP_FILE="/tmp/tmpfile-$SCRIPT_NAME"
@@ -19,7 +21,7 @@ then
 
         # determine PID list of running node-processes from not-develop (not-master and not-audio) branches (branches like as 'feature*' or 'hotfix*' or 'test*' or "bugfix*")
         declare -a LIST_OF_NODE_PID
-        LIST_OF_NODE_PID=($(ps aux | grep node.*current.js | egrep -v "develop|master|audio|grep node" | awk '{print $2}'))
+        LIST_OF_NODE_PID=($(ps aux | grep node.*stage.js | egrep -v "develop|master|audio|grep node" | awk '{print $2}'))
         # if PID list not empty
         if ((${#LIST_OF_NODE_PID[@]}>0))
         then
@@ -44,7 +46,7 @@ then
 
         # determine list of apache config files
         declare -a LIST_OF_APACHE_CONF_FILES
-        LIST_OF_APACHE_CONF_FILES=($(ls /etc/apache2/sites-enabled/irls-current-reader-*  | egrep -v "develop|master|audio"))
+        LIST_OF_APACHE_CONF_FILES=($(ls /etc/apache2/sites-enabled/irls-stage-reader-*  | egrep -v "develop|master|audio"))
         # if list of apache config files not empty
         if ((${#LIST_OF_APACHE_CONF_FILES[@]}>0))
         then
@@ -60,9 +62,9 @@ then
                         if ! netstat -nlpt | grep $PORT_NUMBER
                         then
                                 echo "apache config ${LIST_OF_APACHE_CONF_FILES[$i]} is exists but node process not exist"
-                                ART_DIR=$(grep irls-reader-artifacts ${LIST_OF_APACHE_CONF_FILES[$i]} | awk '{print $3}' | sort | uniq | awk -F "/" '{print $5}')
-                                echo "remove artifact directory $CURRENT_ART_DIR/$ART_DIR"
-                                rm -rf $CURRENT_ART_DIR/$ART_DIR
+                                ART_DIR=$(grep irls-reader-artifacts-stage ${LIST_OF_APACHE_CONF_FILES[$i]} | awk '{print $3}' | sort | uniq | awk -F "/" '{print $5}')
+                                echo "remove artifact directory $STAGE_ART_DIR/$ART_DIR"
+                                rm -rf $STAGE_ART_DIR/$ART_DIR
                                 echo "remove apache config ${LIST_OF_APACHE_CONF_FILES[$i]}"
                                 rm -f ${LIST_OF_APACHE_CONF_FILES[$i]}
                         fi
@@ -73,7 +75,7 @@ then
         # determine list of deployed branches
         declare -a LIST_OF_BRANCHES
         cd /etc/apache2/sites-enabled/
-        LIST_OF_BRANCHES=($(ls irls-current-reader-* | egrep -v "develop|master|audio" | sed -e 's@irls-current-reader-[a-z_-]*-bugfix-@bugfix\/@g' -e 's@irls-current-reader-[a-z_-]*-feature-@feature\/@g' -e 's@irls-current-reader-[a-z_-]*-hotfix-@hotfix\/@g' -e 's@irls-current-reader-[a-z_-]*-test-@test\/@g' | sort | uniq))
+        LIST_OF_BRANCHES=($(ls irls-stage-reader-* | egrep -v "develop|master|audio" | sed -e 's@irls-stage-reader-[a-z_-]*-bugfix-@bugfix\/@g' -e 's@irls-stage-reader-[a-z_-]*-feature-@feature\/@g' -e 's@irls-stage-reader-[a-z_-]*-hotfix-@hotfix\/@g' -e 's@irls-stage-reader-[a-z_-]*-test-@test\/@g' | sort | uniq))
         # if list of branches not empty
         if ((${#LIST_OF_BRANCHES[@]}>0))
         then
@@ -90,7 +92,7 @@ then
                                 echo "branch ${LIST_OF_BRANCHES[$branch]} is not actual ( not contain in the file $BRANCHES_JSON)"
                                 # determine config files with this branch
                                 declare -a LIST_OF_APACHE_CONF_FILES
-                                LIST_OF_APACHE_CONF_FILES=($(ls irls-current-reader-*$(echo ${LIST_OF_BRANCHES[$branch]} | sed 's/\//-/g')))
+                                LIST_OF_APACHE_CONF_FILES=($(ls irls-stage-reader-*$(echo ${LIST_OF_BRANCHES[$branch]} | sed 's/\//-/g')))
                                 LENGTH_OF_LIST=${#LIST_OF_APACHE_CONF_FILES[@]}
                                 if ((${#LIST_OF_BRANCHES[@]}>0))
                                 then
@@ -103,9 +105,9 @@ then
                                                 PID=$(netstat -nlpt | grep $PORT_NUMBER | awk '{print $7}' | sed 's/\/node//g')
                                                 echo "kill node process $PID"
                                                 kill -9 $PID
-                                                ART_DIR=$(grep irls-reader-artifacts ${LIST_OF_APACHE_CONF_FILES[$i]} | awk '{print $3}' | sort | uniq | awk -F "/" '{print $5}')
-                                                echo "remove artifact directory $CURRENT_ART_DIR/$ART_DIR"
-                                                rm -rf $CURRENT_ART_DIR/$ART_DIR
+                                                ART_DIR=$(grep irls-reader-artifacts-stage ${LIST_OF_APACHE_CONF_FILES[$i]} | awk '{print $3}' | sort | uniq | awk -F "/" '{print $5}')
+                                                echo "remove artifact directory $STAGE_ART_DIR/$ART_DIR"
+                                                rm -rf $STAGE_ART_DIR/$ART_DIR
                                                 echo "remove apache config ${LIST_OF_APACHE_CONF_FILES[$i]}"
                                                 rm -f ${LIST_OF_APACHE_CONF_FILES[$i]}
                                         done
@@ -122,25 +124,29 @@ then
 
         # list of apache config files
         cd
-        LIST_APACHE_CONFIGS=$(ls /etc/apache2/sites-enabled/irls-current-reader-*)
-        for apache_config_file in $LIST_APACHE_CONFIGS
-        do
-               # determine for artifacts directory
-               EXCLUDED_ART_DIR=$(grep irls-reader-artifacts $apache_config_file | awk '{print $3}' | uniq | awk -F "/" '{print $5}')
-               COUNT_LINES_TMP_FILE="$(cat $TMP_FILE | wc -l)"
-               # fill tmp-file
-               if [ "$COUNT_LINES_TMP_FILE" = "0" ]; then
-                       echo "ls -oAtrL $CURRENT_ART_DIR | grep  "^d" | grep -v json |"  >> $TMP_FILE
-                       printf  " grep -v $EXCLUDED_ART_DIR |" >> $TMP_FILE
-               else
-                       printf  " grep -v $EXCLUDED_ART_DIR |" >> $TMP_FILE
-               fi
-        done
+        LIST_APACHE_CONFIGS=($(ls /etc/apache2/sites-enabled/irls-stage-reader-*))
+        LENGTH_OF_LIST=${#LIST_APACHE_CONFIGS[@]}
+        if ((${#LIST_APACHE_CONFIGS[@]}>0))
+        then
+                for (( apache_config_file=0; apache_config_file<$LENGTH_OF_LIST; apache_config_file++ ));
+                do
+                        # determine for artifacts directory
+                        EXCLUDED_ART_DIR=$(grep irls-reader-artifacts-stage ${LIST_APACHE_CONFIGS[$apache_config_file]} | awk '{print $3}' | uniq | awk -F "/" '{print $5}')
+                        COUNT_LINES_TMP_FILE="$(cat $TMP_FILE | wc -l)"
+                        # fill tmp-file
+                        if [ "$COUNT_LINES_TMP_FILE" = "0" ]; then
+                                echo "ls -oAtrL $STAGE_ART_DIR | grep  "^d" | grep -v json |"  >> $TMP_FILE
+                                printf  " grep -v $EXCLUDED_ART_DIR |" >> $TMP_FILE
+                        else
+                                printf  " grep -v $EXCLUDED_ART_DIR |" >> $TMP_FILE
+                        fi
+                done
+        fi
         echo " awk '{print \$8}'" >> $TMP_FILE
         LIST_OF_DELETED_DIRS=$(cat $TMP_FILE | tr '\n' ' ' | xargs -0 bash -c)
         COUNT_LINES=$(cat $TMP_FILE | tr '\n' ' ' | xargs -0 bash -c | wc -l)
         # remove of artifacts directories
-        cd $CURRENT_ART_DIR
+        cd $STAGE_ART_DIR
         A=$(cat $TMP_FILE | tr '\n' ' ' | xargs -0 bash -c | tail -"$COUNT_LINES")
         if [ -z $A ]
         then
@@ -152,7 +158,7 @@ then
         echo ""
 
         echo "After:"
-        echo "$CURRENT_ART_DIR used $(du -s $CURRENT_ART_DIR/ | awk '{print $1}') kilobytes"
+        echo "$STAGE_ART_DIR used $(du -s $STAGE_ART_DIR/ | awk '{print $1}') kilobytes"
         /usr/sbin/service apache2 stop
         sleep 1
         /usr/sbin/service apache2 start
