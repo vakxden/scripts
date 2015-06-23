2a91fb02626c256ae0e1a3a2bfc725214bec6111
Denys Vachov <dvac@isd.dp.ua>
2015-06-23 10:26:19 +0300

    fix git clone for job named reader-tests

--- jenkins-irls/reader-tests.sh
+++ jenkins-irls/reader-tests.sh
@@ -1,30 +1,7 @@
-sleep 15
-
-### Variables of repositories
 READER_REPONAME="product"
-
-### Functions for git command
-function git_clone {
-        cd $WORKSPACE
-        git clone git@wpp.isd.dp.ua:irls/$REPONAME.git
-        }
-
-function git_checkout {
-        cd $WORKSPACE/$REPONAME
-        git reset --hard
-        git clean -fdx
-        git fetch --all
-        git checkout origin/$BRANCHNAME
-        }
-
-### Cloning of product-repo
-REPONAME="$READER_REPONAME"
-if [ ! -d $WORKSPACE/$REPONAME ]; then
-        git_clone
-        git_checkout
-else
-        git_checkout
-fi
+rm -rf $WORKSPACE/*
+mkdir -p $WORKSPACE/$READER_REPONAME
+cp -Rf $HOME/irls-reader-current-build/$GIT_COMMIT-$TARGET/* $WORKSPACE/$READER_REPONAME/
 export DISPLAY=:99
 export NODE_HOME=/opt/node
 export PATH=$PATH:$NODE_HOME/bin
@@ -32,7 +9,7 @@ export PATH=$PATH:$NODE_HOME/bin
 ENV_CONFIG="$WORKSPACE/$READER_REPONAME/tests/data/environmentConfig.json"
 cat $ENV_CONFIG
 cat /dev/null > $ENV_CONFIG
-echo -e '{\n"url":"http://irls-autotests.design.isd.dp.ua/irls/test/reader/autotest/develop/"\n}' >> $ENV_CONFIG
+echo -e '{\n"url":"https://wpps.isd.dp.ua/irls/stage/reader/ffa/develop/"\n}' >> $ENV_CONFIG
 cat $ENV_CONFIG
 ### Running tests
 cd $WORKSPACE/$READER_REPONAME/tests/spec
@@ -45,10 +22,10 @@ cd $WORKSPACE/$READER_REPONAME/tests/spec
 grunt
 ### Archiving test results and screenshots
 ssh jenkins@dev01.isd.dp.ua "if [ ! -d /var/lib/jenkins/jobs/$JOB_NAME/builds/$BUILD_ID/archive ]; then mkdir -p /var/lib/jenkins/jobs/$JOB_NAME/builds/$BUILD_ID/archive/{reports,screenshots}; fi"
-scp $WORKSPACE/product/tests/reports/*.xml jenkins@dev01.isd.dp.ua:/var/lib/jenkins/jobs/$JOB_NAME/builds/$BUILD_ID/archive/reports/
-if [ "$(ls -A $WORKSPACE/product/tests/screenshots)" ]; then
-    echo "Directory $WORKSPACE/product/tests/screenshots is not empty"
-	scp $WORKSPACE/product/tests/screenshots/*.png jenkins@dev01.isd.dp.ua:/var/lib/jenkins/jobs/$JOB_NAME/builds/$BUILD_ID/archive/screenshots/
+scp $WORKSPACE/$READER_REPONAME/tests/reports/*.xml jenkins@dev01.isd.dp.ua:/var/lib/jenkins/jobs/$JOB_NAME/builds/$BUILD_ID/archive/reports/
+if [ "$(ls -A $WORKSPACE/$READER_REPONAME/tests/screenshots)" ]; then
+    echo "Directory $WORKSPACE/$READER_REPONAME/tests/screenshots is not empty"
+	scp $WORKSPACE/$READER_REPONAME/tests/screenshots/*.png jenkins@dev01.isd.dp.ua:/var/lib/jenkins/jobs/$JOB_NAME/builds/$BUILD_ID/archive/screenshots/
 else
-    echo "directory $WORKSPACE/product/tests/screenshots is empty"
+    echo "directory $WORKSPACE/$READER_REPONAME/tests/screenshots is empty"
 fi
